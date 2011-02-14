cb03eca4ad0b75c09b3d77fa3db312ec128c29ca
0xC0000054 <0xC0000054@users.noreply.github.com>
2011-02-14 04:42:46 +0000

    initial commit

---
+++ PSFilterShim/PSApi/Int32Extensions.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace PSFilterLoad.PSApi
+{
+    static class Int32Extensions
+    {
+        public static int Clamp(this int i, int min, int max)
+        {
+            if (i < min)
+            {
+                i = min;
+            }
+             
+            if (i > max)
+	        {
+		       i = max;
+	        }
+
+            return i;
+        }
+    }
+}
