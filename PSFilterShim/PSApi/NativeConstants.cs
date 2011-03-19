a42fa872d8075ffd29c59ea35334c65445c372a2
0xC0000054 <0xC0000054@users.noreply.github.com>
2011-03-19 05:27:06 +0000

    Added the GPTR GlobalAlloc flag.

--- PSFilterShim/PSApi/NativeConstants.cs
+++ PSFilterShim/PSApi/NativeConstants.cs
@@ -2,6 +2,8 @@
 {
     static class NativeConstants
     {
+        public const int GPTR = 64;
+
         /// GMEM_MOVEABLE -> 0x0002
         public const int GMEM_MOVEABLE = 2;
         /// LOAD_LIBRARY_AS_DATAFILE -> 0x00000002
