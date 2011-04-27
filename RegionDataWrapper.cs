10e0906e73c14c09901c23c047ef95d0d670ce57
0xC0000054 <0xC0000054@users.noreply.github.com>
2011-04-27 20:15:23 +0000

    Fix Code Analysis warnings.

--- RegionDataWrapper.cs
+++ RegionDataWrapper.cs
@@ -2,6 +2,7 @@
 using System.Drawing;
 using System.Drawing.Drawing2D;
 using System.Runtime.Serialization;
+using System.Security.Permissions;
 
 namespace PSFilterPdn
 {
@@ -15,7 +16,7 @@ namespace PSFilterPdn
             this.rgnData = rgn.Data;
         }
 
-        public RegionDataWrapper(SerializationInfo info, StreamingContext context)
+        private RegionDataWrapper(SerializationInfo info, StreamingContext context)
         {
             this.rgnData = (byte[])info.GetValue("rgnData", typeof(byte[]));
         }
@@ -28,7 +29,7 @@ namespace PSFilterPdn
             }
         }
 
-
+        [SecurityPermission(SecurityAction.LinkDemand, Flags = SecurityPermissionFlag.SerializationFormatter)]
         public void GetObjectData(SerializationInfo info, StreamingContext context)
         {
             info.AddValue("rgnData", this.rgnData, typeof(byte[]));
