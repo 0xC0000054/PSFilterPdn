0639d9653517a595ea9d4cc6bc8bd0f6b5c6cec3
0xC0000054 <0xC0000054@users.noreply.github.com>
2011-04-27 04:46:52 +0000

    Initial Commit

---
+++ RegionDataWrapper.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Drawing;
+using System.Drawing.Drawing2D;
+using System.Runtime.Serialization;
+
+namespace PSFilterPdn
+{
+    [Serializable]
+    public sealed class RegionDataWrapper : ISerializable
+    {
+        private byte[] rgnData;
+
+        internal RegionDataWrapper(RegionData rgn)
+        {
+            this.rgnData = rgn.Data;
+        }
+
+        public RegionDataWrapper(SerializationInfo info, StreamingContext context)
+        {
+            this.rgnData = (byte[])info.GetValue("rgnData", typeof(byte[]));
+        }
+
+        public byte[] Data
+        {
+            get
+            {
+                return rgnData;
+            }
+        }
+
+
+        public void GetObjectData(SerializationInfo info, StreamingContext context)
+        {
+            info.AddValue("rgnData", this.rgnData, typeof(byte[]));
+        }
+    }
+}
