d0facd469cd793a1798cd5759f3adf3285d38e5c
0xC0000054 <0xC0000054@users.noreply.github.com>
2011-06-24 02:12:54 +0000

    Changed the LOAD_LIBRARY_AS_DATAFILE constant to an uint.

--- PSApi/NativeConstants.cs
+++ PSApi/NativeConstants.cs
@@ -7,7 +7,7 @@
         /// GMEM_MOVEABLE -> 0x0002
         public const int GMEM_MOVEABLE = 2;
         /// LOAD_LIBRARY_AS_DATAFILE -> 0x00000002
-        public const int LOAD_LIBRARY_AS_DATAFILE = 2;
+        public const uint LOAD_LIBRARY_AS_DATAFILE = 2;
         public const int RT_DIALOG = 5;
 
 
