61cd0f25210e3cd64b8c194d747ac452863ab06d
0xC0000054 <0xC0000054@users.noreply.github.com>
2011-07-07 04:34:07 +0000

    Initial Commit

---
+++ PSApi/DictonaryExtensions.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace PSFilterLoad.PSApi
+{
+    public static class DictonaryExtensions
+    {
+        public static void AddOrUpdate<TKey, TValue>(this IDictionary<TKey, TValue> dict, TKey key, TValue value)
+        {
+            if (!dict.ContainsKey(key))
+            {
+                dict.Add(key, value);
+            }
+            else
+            {
+                dict[key] = value;
+            }
+        }
+    }
+}
