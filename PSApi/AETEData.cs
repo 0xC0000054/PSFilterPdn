d0a974ec4f5870692ea56bb0550cb2d5859f8989
0xC0000054 <0xC0000054@users.noreply.github.com>
2011-07-07 04:34:37 +0000

    Initial Commit

---
+++ PSApi/AETEData.cs
@@ -0,0 +1,82 @@
+﻿using System.Collections.Generic;
+namespace PSFilterLoad.PSApi
+{
+
+#if PSSDK4
+    internal struct AETEParm
+    {
+        public string name;
+        public uint key;
+        public uint type;
+        public string desc;
+        public short flags;
+    }
+
+    internal struct AETEEnums
+    {
+        public uint type;
+        public short count;
+        public AETEEnum[] enums;
+    }
+
+    internal struct AETEEnum
+    {
+        public string name;
+        public uint type;
+        public string desc;
+    }
+
+    internal struct AETEEvent
+    {
+        public string vendor;
+        public string desc;
+        public int evntClass;
+        public int type;
+        public uint replyType;
+        public uint parmType;
+        public short flags;
+        public AETEParm[] parms;
+        public AETEEnums[] enums;
+    }
+
+    internal sealed class PluginAETE
+    {
+        public short version;
+        public short lang;
+        public string suiteID;
+        public short suiteLevel;
+        public short suiteVersion;
+
+        public AETEEvent[] events;
+    }
+
+    [System.Serializable()]
+    public sealed class AETEData
+    {
+        private Dictionary<uint, short> flagList;
+
+        public Dictionary<uint, short> FlagList
+        {
+            get
+            {
+                return flagList;
+            }
+        }
+
+        internal AETEData(PluginAETE enumAETE)
+        {
+            this.flagList = new Dictionary<uint, short>();
+            foreach (var item in enumAETE.events)
+            {
+                for (int f = 0; f < item.parms.Length; f++)
+                {
+                    AETEParm parm = item.parms[f];
+                    flagList.Add(parm.key, parm.flags);
+                }
+            }
+        }
+    } 
+#endif
+    
+
+}
