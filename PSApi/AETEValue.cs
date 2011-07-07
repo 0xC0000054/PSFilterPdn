2c562b55ee0b928e337e501e8bd441dc17b9eda1
0xC0000054 <0xC0000054@users.noreply.github.com>
2011-07-07 04:34:42 +0000

    Initial Commit

---
+++ PSApi/AETEValue.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace PSFilterLoad.PSApi
+{
+#if PSSDK4
+    [Serializable]
+	public struct AETEValue
+    {
+        public uint type;
+        public int flags;
+        public int size;
+        public object value;
+
+        public AETEValue(uint type, int flags, int size, object value)
+        {
+            this.type = type;
+            this.flags = flags;
+            this.size = size;
+            this.value = value;
+        }
+    } 
+  
+#endif
+}
