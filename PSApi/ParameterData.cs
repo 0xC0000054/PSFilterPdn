dfdd10d7de19776ec309cbe353a8669c8c9c1e67
0xC0000054 <0xC0000054@users.noreply.github.com>
2011-07-12 03:33:25 +0000

    Re-written to support the AETE scripting data.

---
+++ PSApi/ParameterData.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Runtime.Serialization;
+
+namespace PSFilterLoad.PSApi
+{
+    /// <summary>
+    /// The struct that holds the saved filter global parameter data.
+    /// </summary>
+    [Serializable]
+    public sealed class ParameterData 
+    {
+        private GlobalParameters globalParms;
+        private Dictionary<uint, AETEValue> aeteDict;
+
+        public GlobalParameters GlobalParms
+        {
+            get 
+            {
+                return globalParms;
+            }
+        }
+
+        public Dictionary<uint, AETEValue> AETEDict
+        {
+            get
+            {
+                return aeteDict;
+            }
+        }
+
+        public ParameterData(GlobalParameters globals, Dictionary<uint, AETEValue> aete)
+        {
+            this.globalParms = globals;
+
+            if (aete != null)
+            {
+                this.aeteDict = new Dictionary<uint, AETEValue>(aete);
+            }
+            else
+            {
+                this.aeteDict = null;
+            }
+        }
+       
+    }
+}
