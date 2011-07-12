6eb8c2be14cf62ff512244aec19edcdbc5d26ec3
0xC0000054 <0xC0000054@users.noreply.github.com>
2011-07-12 00:52:10 +0000

    Removed the PSImageMultiPlane.

--- PSFilterShim/PSApi/PSImagePlane.cs
+++ PSFilterShim/PSApi/PSImagePlane.cs
@@ -4,7 +4,7 @@ using System.Runtime.InteropServices;
 namespace PSFilterLoad.PSApi
 {
 
-
+#if USEIMAGESERVICES
     [StructLayout(LayoutKind.Sequential)]
     internal struct PSImagePlane
     {
@@ -20,24 +20,6 @@ namespace PSFilterLoad.PSApi
 
         /// int32->int
         public int colBytes;
-    }
-    [StructLayout(LayoutKind.Sequential)]
-    internal struct PSImageMultiPlane
-    {
-
-        /// void*
-        public IntPtr data;
-
-        /// Rect
-        public Rect16 bounds;
-
-        /// int32->int
-        public int rowBytes;
-
-        /// int32->int
-        public int colBytes;
-
-        /// int16->short
-        public short activeChannels;
-    }
+    } 
+#endif
 }
