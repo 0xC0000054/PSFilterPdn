8a5f92e9030559d2750db4c76f24448f46fff86f
0xC0000054 <0xC0000054@users.noreply.github.com>
2011-07-30 06:55:37 +0000

    Modified to use properties.

--- PSApi/AETEValue.cs
+++ PSApi/AETEValue.cs
@@ -6,10 +6,42 @@ namespace PSFilterLoad.PSApi
     [Serializable]
 	public struct AETEValue
     {
-        public uint type;
-        public int flags;
-        public int size;
-        public object value;
+        private uint type;
+        private int flags;
+        private int size;
+        private object value;
+
+        public uint Type
+        {
+            get
+            {
+                return type;
+            }
+        }
+
+        public int Flags
+        {
+            get
+            {
+                return flags;
+            }
+        }
+
+        public int Size
+        {
+            get
+            {
+                return size;
+            }
+        }
+
+        public object Value
+        {
+            get
+            {
+                return value;
+            }
+        }
 
         public AETEValue(uint type, int flags, int size, object value)
         {
