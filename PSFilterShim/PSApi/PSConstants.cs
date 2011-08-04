1e10f63da917a7eb533221b5ed5f2eca749dedb7
0xC0000054 <0xC0000054@users.noreply.github.com>
2011-08-04 05:18:12 +0000

    Added the interpolation mode and channel name properties.

--- PSFilterShim/PSApi/PSConstants.cs
+++ PSFilterShim/PSApi/PSConstants.cs
@@ -61,9 +61,18 @@ namespace PSFilterLoad.PSApi
         /// </summary>
         public const uint propNumberOfChannels = 0x6e756368U;
         /// <summary>
+        /// Channel Name - 'nmch'
+        /// </summary>
+        public const uint propChannelName = 0x6e6d6368U;
+        /// <summary>
         /// Image mode - 'mode'
         /// </summary>
         public const uint propImageMode = 0x6d6f6465U;
+        /// <summary>
+        /// Interplolation Mode - 'intp',
+        /// The current interpolation method: 1 = point sample, 2 = bilinear, 3 = bicubic
+        /// </summary>
+        public const uint propInterpolationMethod = 0x696E7470U;
         /// <summary>
         /// The fourth bit in the first byte is RGB.
         /// </summary>
