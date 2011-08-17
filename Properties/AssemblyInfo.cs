f4357c5e3319fe23bd6a8cd13d6f5b721978c808
0xC0000054 <0xC0000054@users.noreply.github.com>
2011-08-17 05:19:21 +0000

    File version updated to 1.0.4.0.

--- Properties/AssemblyInfo.cs
+++ Properties/AssemblyInfo.cs
@@ -28,5 +28,5 @@ using System.Runtime.InteropServices;
 //      Build Number
 //      Revision
 //
-[assembly: AssemblyVersion("1.0.3.9")]
-[assembly: AssemblyFileVersion("1.0.3.9")]
+[assembly: AssemblyVersion("1.0.4.0")]
+[assembly: AssemblyFileVersion("1.0.4.0")]
