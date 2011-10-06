b3779ab8230ed2330794e811278de82785e6d6d6
0xC0000054 <0xC0000054@users.noreply.github.com>
2011-10-06 05:04:11 +0000

    Removed the unused GDI BitmapInfo structs.

--- PSFilterShim/PSApi/NativeStructs.cs
+++ PSFilterShim/PSApi/NativeStructs.cs
@@ -4,39 +4,6 @@ namespace PSFilterLoad.PSApi
 {
     static class NativeStructs
     {
-        [StructLayoutAttribute(LayoutKind.Sequential)]
-        public struct BITMAPINFO
-        {
-            public BITMAPINFOHEADER bmiHeader;
-            [MarshalAsAttribute(UnmanagedType.ByValArray, SizeConst = 1, ArraySubType = UnmanagedType.Struct)]
-            public RGBQUAD[] bmiColors;
-        }
-
-        [StructLayoutAttribute(LayoutKind.Sequential)]
-        public struct BITMAPINFOHEADER
-        {
-            public uint biSize;
-            public int biWidth;
-            public int biHeight;
-            public ushort biPlanes;
-            public ushort biBitCount;
-            public uint biCompression;
-            public uint biSizeImage;
-            public int biXPelsPerMeter;
-            public int biYPelsPerMeter;
-            public uint biClrUsed;
-            public uint biClrImportant;
-        }
-
-        [StructLayoutAttribute(LayoutKind.Sequential)]
-        public struct RGBQUAD
-        {
-            public byte rgbBlue;
-            public byte rgbGreen;
-            public byte rgbRed;
-            public byte rgbReserved;
-        }
-
         [System.Runtime.InteropServices.StructLayoutAttribute(System.Runtime.InteropServices.LayoutKind.Sequential)]
         public struct MEMORY_BASIC_INFORMATION
         {
