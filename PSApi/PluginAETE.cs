4de46f2d46b04b9fb6ac759ac25eed5bb6011129
0xC0000054 <0xC0000054@users.noreply.github.com>
2011-10-11 06:39:19 +0000

    Initial Commit

---
+++ PSApi/PluginAETE.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace PSFilterPdn.PSApi
+{
+#if PSSDK4
+    internal struct AETEParm
+    {
+        public string name;
+        public uint key;
+        public uint type;
+        public string desc;
+        public short flags;
+    }
+
+    internal struct AETEEnums
+    {
+        public uint type;
+        public short count;
+        public AETEEnum[] enums;
+    }
+
+    internal struct AETEEnum
+    {
+        public string name;
+        public uint type;
+        public string desc;
+    }
+
+    internal struct AETEEvent
+    {
+        public string vendor;
+        public string desc;
+        public int evntClass;
+        public int type;
+        public uint replyType;
+        public uint parmType;
+        public short flags;
+        public AETEParm[] parms;
+        public AETEEnums[] enums;
+    }
+
+    internal sealed class PluginAETE
+    {
+        public short version;
+        public short lang;
+        public string suiteID;
+        public short suiteLevel;
+        public short suiteVersion;
+
+        public AETEEvent[] events;
+    } 
+#endif
+}
