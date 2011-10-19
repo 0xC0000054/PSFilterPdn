bab69abfb2cc78a0e35afd96f28dd678de63eb88
0xC0000054 <0xC0000054@users.noreply.github.com>
2011-10-19 04:13:13 +0000

    Documentation updates.

--- PSFilterShim/PSApi/ImageSizeTooLargeException.cs
+++ PSFilterShim/PSApi/ImageSizeTooLargeException.cs
@@ -3,12 +3,22 @@ using System.Runtime.Serialization;
 
 namespace PSFilterLoad.PSApi
 {
+    /// <summary>
+    /// The exception that occurs when the image size exceeds 32000 pixels.
+    /// </summary>
     [Serializable]
     class ImageSizeTooLargeException : Exception, ISerializable 
     {
+        /// <summary>
+        /// Initializes a new instance of the <see cref="ImageSizeTooLargeException"/> class.
+        /// </summary>
         public ImageSizeTooLargeException() : base("An ImageSizeTooLargeException has occured")
         {
         }
+        /// <summary>
+        /// Initializes a new instance of the <see cref="ImageSizeTooLargeException"/> class.
+        /// </summary>
+        /// <param name="message">The message to display.</param>
         public ImageSizeTooLargeException(string message) : base(message)
         {
         }
