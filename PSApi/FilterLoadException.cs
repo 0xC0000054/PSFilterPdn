015d2c7a57438d0cf0a38edd3d50b4f265ac261c
0xC0000054 <0xC0000054@users.noreply.github.com>
2011-10-19 04:12:01 +0000

    Documentation updates.

--- PSApi/FilterLoadException.cs
+++ PSApi/FilterLoadException.cs
@@ -3,13 +3,23 @@ using System.Runtime.Serialization;
 
 namespace PSFilterLoad.PSApi
 {
+    /// <summary>
+    /// The exception thrown when loading the PiPL or PiMI resources fails.
+    /// </summary>
     [Serializable]
     class FilterLoadException : Exception, ISerializable
     {
+        /// <summary>
+        /// Initializes a new instance of the <see cref="FilterLoadException"/> class.
+        /// </summary>
         public FilterLoadException() : base("A FilterLoadException has occured")
         {
             
         }
+        /// <summary>
+        /// Initializes a new instance of the <see cref="FilterLoadException"/> class.
+        /// </summary>
+        /// <param name="message">The message to display.</param>
         public FilterLoadException(string message) : base(message)
         {
             
