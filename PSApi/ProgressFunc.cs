03d95d3a9f4a2aa947adf0069910f3cdd1b5dc0d
0xC0000054 <0xC0000054@users.noreply.github.com>
2011-10-19 04:13:01 +0000

    Documentation updates.

--- PSApi/ProgressFunc.cs
+++ PSApi/ProgressFunc.cs
@@ -1,9 +1,10 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-
+﻿
 namespace PSFilterLoad.PSApi
-{
+{ 
+    /// <summary>
+    /// The callback that reports the render progress to PDN.
+    /// </summary>
+    /// <param name="done">The amount of work done.</param>
+    /// <param name="total">The total amout of work.</param>
     internal delegate void ProgressFunc(int done, int total);
 }
