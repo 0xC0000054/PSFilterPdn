fa881a670d604dbfd31e38a734666df435db058e
0xC0000054 <0xC0000054@users.noreply.github.com>
2011-12-02 20:09:35 +0000

    Replace LocalAlloc with HeapAlloc.

--- PSFilterShim/PSApi/NativeMethods.cs
+++ PSFilterShim/PSApi/NativeMethods.cs
@@ -40,19 +40,21 @@ namespace PSFilterLoad.PSApi
         [return: MarshalAs(UnmanagedType.Bool)]
         public static extern bool SetWindowText(IntPtr hWnd, string lpString);
 
-        [DllImport("kernel32.dll", CharSet = CharSet.Unicode, SetLastError = true)]
-        public static extern IntPtr LocalAlloc([In] uint uFlags, [In] IntPtr sizetdwBytes);
+        [DllImport("kernel32.dll", SetLastError = true)]
+        internal static extern IntPtr GetProcessHeap();
 
-        [DllImport("kernel32.dll", SetLastError = true, ExactSpelling = true)]
-        public static extern IntPtr LocalFree(IntPtr hMem);
+        [DllImport("kernel32.dll", SetLastError = true)]
+        internal static extern IntPtr HeapAlloc(IntPtr hHeap, uint dwFlags, UIntPtr dwSize);
 
-        [DllImport("kernel32.dll", EntryPoint = "LocalSize")]
-        public static extern UIntPtr LocalSize([In()] System.IntPtr hMem);
+        [DllImport("kernel32.dll", SetLastError = true)]
+        internal static extern bool HeapFree(IntPtr hHeap, uint dwFlags, IntPtr lpMem);
 
-        /// Return Type: SIZE_T->ULONG_PTR->unsigned int
-        ///lpAddress: LPCVOID->void*
-        ///lpBuffer: PMEMORY_BASIC_INFORMATION->_MEMORY_BASIC_INFORMATION*
-        ///dwLength: SIZE_T->ULONG_PTR->unsigned int
+        [DllImport("kernel32.dll")]
+        internal static extern IntPtr HeapReAlloc(IntPtr hHeap, uint dwFlags, IntPtr lpMem, UIntPtr dwBytes);
+
+        [DllImport("kernel32.dll")]
+        internal static extern UIntPtr HeapSize(IntPtr hHeap, uint dwFlags, IntPtr lpMem);
+
         [DllImport("kernel32.dll", SetLastError = true)]
         public static extern unsafe IntPtr VirtualQuery(IntPtr address, ref NativeStructs.MEMORY_BASIC_INFORMATION buffer, IntPtr sizeOfBuffer);
 
