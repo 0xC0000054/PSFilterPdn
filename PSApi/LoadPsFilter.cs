d86795d7113c0c0b3d2cf48f68b738a87caeea4c
0xC0000054 <0xC0000054@users.noreply.github.com>
2011-12-07 08:34:27 +0000

    Set the readDiscriptorPtr to IntPtr.Zero when it is disposed.

--- PSApi/LoadPsFilter.cs
+++ PSApi/LoadPsFilter.cs
@@ -5001,7 +5001,7 @@ namespace PSFilterLoad.PSApi
                     if (readDescriptorPtr != IntPtr.Zero)
                     {
                         Memory.Free(readDescriptorPtr);
-                        property_procsPtr = IntPtr.Zero;
+                        readDescriptorPtr = IntPtr.Zero;
                     }
                     if (writeDescriptorPtr != IntPtr.Zero)
                     {
