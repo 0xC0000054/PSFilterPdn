c238e3e10cd98371ab39071cf61193dd73478446
0xC0000054 <0xC0000054@users.noreply.github.com>
2011-12-12 08:20:42 +0000

    Updated assembly description.

--- PSFilterShim/Properties/AssemblyInfo.cs
+++ PSFilterShim/Properties/AssemblyInfo.cs
@@ -4,7 +4,7 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("Shim program to run 32-bit filters on a 64-bit OS.")]
+[assembly: AssemblyTitle("Shim program to run 32-bit Photoshop® filters on a 64-bit OS.")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("null54")]
