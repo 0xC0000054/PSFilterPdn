ca95fabce43cb0a66e5bb3737d3dd6c11e0a7c14
0xC0000054 <0xC0000054@users.noreply.github.com>
2011-12-18 08:30:23 +0000

    Updated to work with WCF.

--- PSFilterShim/PSApi/PluginData.cs
+++ PSFilterShim/PSApi/PluginData.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Runtime.InteropServices;
+using System.Runtime.Serialization;
 
 namespace PSFilterLoad.PSApi
 {
@@ -8,18 +9,30 @@ namespace PSFilterLoad.PSApi
     /// <summary>
     /// The class that encapsulates a Photoshop Filter plugin
     /// </summary>
+    [DataContract()]
     internal sealed class PluginData
     {
+        [DataMember]
         public string fileName;
+        [DataMember]
         public string entryPoint;
+        [DataMember]
         public string category;
+        [DataMember]
         public string title;
+        [DataMember]
         public FilterCaseInfo[] filterInfo;
-        public AETEData aete;
         /// <summary>
         /// The structure containing the dll entrypoint
         /// </summary>
         public PIEntrypoint entry;
+        /// <summary>
+        /// Used to run 32-bit plugins in 64-bit Paint.NET
+        /// </summary>
+        [DataMember]
+        public bool runWith32BitShim;
+        [DataMember]
+        public AETEData aete;
     }
 
     internal struct PIEntrypoint
