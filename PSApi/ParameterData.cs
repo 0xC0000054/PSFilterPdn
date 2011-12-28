9e1a6a3e4d49edac9e0d04a4b850b7b943e949bc
0xC0000054 <0xC0000054@users.noreply.github.com>
2011-12-28 07:27:33 +0000

    Removed WCF support.

--- PSApi/ParameterData.cs
+++ PSApi/ParameterData.cs
@@ -8,12 +8,9 @@ namespace PSFilterLoad.PSApi
     /// The struct that holds the saved filter global parameter data.
     /// </summary>
     [Serializable]
-    [DataContract]
     public sealed class ParameterData 
     {
-        [DataMember]
         private GlobalParameters globalParameters;
-        [DataMember]
         private Dictionary<uint, AETEValue> aeteDict;
 
         public GlobalParameters GlobalParameters
