5e23e6fbbab9bb7847d83376d7195b93e0712bd4
0xC0000054 <0xC0000054@users.noreply.github.com>
2012-01-21 05:07:17 +0000

    Initial Commit.

---
+++ PluginSupportInfo.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Reflection;
+using PaintDotNet;
+
+namespace PSFilterPdn
+{
+    public sealed class PluginSupportInfo : IPluginSupportInfo
+    {
+        public string Author
+        {
+            get 
+            {
+                return "null54"; 
+            }
+        }
+
+        public string Copyright
+        {
+            get
+            {
+                return ((AssemblyCopyrightAttribute)base.GetType().Assembly.GetCustomAttributes(typeof(AssemblyCopyrightAttribute), false)[0]).Copyright; 
+            }
+        }
+
+        public string DisplayName
+        {
+            get
+            {
+                return PSFilterPdnEffect.StaticName;
+            }
+        }
+
+        public Version Version
+        {
+            get 
+            {
+                return base.GetType().Assembly.GetName().Version;
+            }
+        }
+
+        public Uri WebsiteUri
+        {
+            get 
+            {
+                return new Uri("http://www.getpaint.net/redirect/plugins.html");
+            }
+        }
+    }
+}
