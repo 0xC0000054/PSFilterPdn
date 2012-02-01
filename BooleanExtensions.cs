69d23369819feb6026a9628670aad9b4db32d9bb
0xC0000054 <0xC0000054@users.noreply.github.com>
2012-02-01 22:29:53 +0000

    Initial Commit.

---
+++ BooleanExtensions.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace PSFilterPdn
+{
+    static class BooleanExtensions
+    {
+        public static byte ToByte(this bool value)
+        {
+            if (value)
+            {
+                return 1;
+            }
+            return 0;
+        }
+    }
+}
