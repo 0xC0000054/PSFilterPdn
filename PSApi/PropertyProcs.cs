27b0da59f5aae5623dce1ce1a8f8cd94d23eb9ae
0xC0000054 <0xC0000054@users.noreply.github.com>
2012-02-01 08:22:33 +0000

    Moved the get / set property delegates into this file.

--- PSApi/PropertyProcs.cs
+++ PSApi/PropertyProcs.cs
@@ -8,6 +8,25 @@ using System.Runtime.InteropServices;
 
 namespace PSFilterLoad.PSApi
 {
+    /// Return Type: OSErr->short
+    ///signature: PIType->unsigned int
+    ///key: PIType->unsigned int
+    ///index: int32->int
+    ///simpleProperty: int32*
+    ///complexProperty: Handle*
+    [UnmanagedFunctionPointer(CallingConvention.Cdecl)]
+    internal delegate short GetPropertyProc(uint signature, uint key, int index, ref int simpleProperty, ref System.IntPtr complexProperty);
+
+    /// Return Type: OSErr->short
+    ///signature: PIType->unsigned int
+    ///key: PIType->unsigned int
+    ///index: int32->int
+    ///simpleProperty: int32->int
+    ///complexProperty: Handle->LPSTR*
+    [UnmanagedFunctionPointer(CallingConvention.Cdecl)]
+    internal delegate short SetPropertyProc(uint signature, uint key, int index, int simpleProperty, ref System.IntPtr complexProperty);
+
+
     [System.Diagnostics.CodeAnalysis.SuppressMessage("Microsoft.Design", "CA1049:TypesThatOwnNativeResourcesShouldBeDisposable"), StructLayout(LayoutKind.Sequential)]
     internal struct PropertyProcs
     {
