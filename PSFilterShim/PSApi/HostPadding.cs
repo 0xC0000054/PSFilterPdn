25fe7e57d71fdb9763873d7e151f7cb01eda241f
0xC0000054 <0xC0000054@users.noreply.github.com>
2012-03-05 04:42:56 +0000

    Invalid Commit 3.

---
+++ PSFilterShim/PSApi/HostPadding.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace PSFilterLoad.PSApi
+{
+    /// <summary>
+    /// The padding values used by the FilterRecord inputPadding and maskPadding.
+    /// </summary>
+    internal enum HostPadding : short
+    {
+        plugInWantsEdgeReplication = -1,
+        plugInDoesNotWantPadding = -2,
+        plugInWantsErrorOnBoundsException = -3
+    }
+}
