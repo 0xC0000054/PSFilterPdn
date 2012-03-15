3afa0b2225a4f65063aa86431c9f824a1b492e69
0xC0000054 <0xC0000054@users.noreply.github.com>
2012-03-15 06:12:37 +0000

    Initial Commit.

---
+++ PSApi/PSResource.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace PSFilterLoad.PSApi
+{
+    [Serializable]
+    public sealed class PSResource
+    {
+        private uint key;
+        private int index;
+        private byte[] data;
+
+        /// <summary>
+        /// Gets the resource key.
+        /// </summary> 
+        public uint Key
+        {
+            get
+            {
+                return key;
+            }
+        }
+
+        /// <summary>
+        /// Gets the resource index.
+        /// </summary>
+        public int Index
+        {
+            get
+            {
+                return index;
+            }
+            set
+            {
+                index = value;
+            }
+        }
+
+        /// <summary>
+        /// Gets the resource data.
+        /// </summary>
+        /// <returns></returns>
+        public byte[] GetData()
+        {
+            return data;
+        }
+
+        public PSResource(uint key, int index, byte[] data)
+        {
+            this.key = key;
+            this.index = index;
+            this.data = data;
+        }
+
+
+        public bool Equals(uint key, int index)
+        {
+            return (this.key == key && this.index == index);
+        }
+    }
+}
