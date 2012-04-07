e44f194d214fc94e5856926ccabbecd2a43a718c
0xC0000054 <0xC0000054@users.noreply.github.com>
2012-04-07 19:33:35 +0000

    Documentation update.

--- ShellLinkCoClass.cs
+++ ShellLinkCoClass.cs
@@ -7,7 +7,7 @@ using System.Runtime.InteropServices;
 namespace PSFilterPdn
 {
     /// <summary>
-    /// The ShellLink CoCreate class ues by the framework for interop
+    /// The ShellLink CoCreate class used by the framework for interop
     /// </summary>
     [ComImport(), Guid("00021401-0000-0000-C000-000000000046")]    
     class ShellLinkCoClass
