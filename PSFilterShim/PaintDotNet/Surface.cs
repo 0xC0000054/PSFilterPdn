f749e4fac61fc971a8ea782103343b00dbae3061
0xC0000054 <0xC0000054@users.noreply.github.com>
2012-07-07 01:31:44 +0000

    Added the BiCubic resampling mode.

--- PSFilterShim/PaintDotNet/Surface.cs
+++ PSFilterShim/PaintDotNet/Surface.cs
@@ -182,7 +182,324 @@ namespace PaintDotNet
             return (ColorBgra*)(((byte*)scan0.VoidStar) + (y * stride));
         }
 
-        public unsafe void FitSurface(Surface source)
+        private double CubeClamped(double x)
+        {
+            if (x >= 0)
+            {
+                return x * x * x;
+            }
+            else
+            {
+                return 0;
+            }
+        }
+
+        /// <summary>
+        /// Implements R() as defined at http://astronomy.swin.edu.au/%7Epbourke/colour/bicubic/
+        /// </summary>
+        private double R(double x)
+        {
+            return (CubeClamped(x + 2) - (4 * CubeClamped(x + 1)) + (6 * CubeClamped(x)) - (4 * CubeClamped(x - 1))) / 6;
+        }
+
+        /// <summary>
+        /// Fits the source surface to this surface using bicubic interpolation.
+        /// </summary>
+        /// <param name="source">The Surface to read pixels from.</param>
+        /// <param name="dstRoi">The rectangle to clip rendering to.</param>
+        /// <remarks>
+        /// This method was implemented with correctness, not performance, in mind. 
+        /// Based on: "Bicubic Interpolation for Image Scaling" by Paul Bourke,
+        ///           http://astronomy.swin.edu.au/%7Epbourke/colour/bicubic/
+        /// </remarks>
+        public void BicubicFitSurface(Surface source)
+        {
+            float leftF = (1 * (float)(width - 1)) / (float)(source.width - 1);
+            float topF = (1 * (height - 1)) / (float)(source.height - 1);
+            float rightF = ((float)(source.width - 3) * (float)(width - 1)) / (float)(source.width - 1);
+            float bottomF = ((float)(source.Height - 3) * (float)(height - 1)) / (float)(source.height - 1);
+
+            int left = (int)Math.Ceiling((double)leftF);
+            int top = (int)Math.Ceiling((double)topF);
+            int right = (int)Math.Floor((double)rightF);
+            int bottom = (int)Math.Floor((double)bottomF);
+
+            Rectangle[] rois = new Rectangle[] {
+                                                   Rectangle.FromLTRB(left, top, right, bottom),
+                                                   new Rectangle(0, 0, width, top),
+                                                   new Rectangle(0, top, left, height - top),
+                                                   new Rectangle(right, top, width - right, height - top),
+                                                   new Rectangle(left, bottom, right - left, height - bottom)
+                                               };
+            Rectangle dstRoi = this.Bounds;
+            for (int i = 0; i < rois.Length; ++i)
+            {
+                rois[i].Intersect(dstRoi);
+
+                if (rois[i].Width > 0 && rois[i].Height > 0)
+                {
+                    if (i == 0)
+                    {
+                        BicubicFitSurfaceUnchecked(source, rois[i]);
+                    }
+                    else
+                    {
+                        BicubicFitSurfaceChecked(source, rois[i]);
+                    }
+                }
+            }
+        }
+
+        /// <summary>
+        /// Implements bicubic filtering with bounds checking at every pixel.
+        /// </summary>
+        private unsafe void BicubicFitSurfaceChecked(Surface source, Rectangle dstRoi)
+        {
+            Rectangle roi = Rectangle.Intersect(dstRoi, this.Bounds);
+            Rectangle roiIn = Rectangle.Intersect(dstRoi, new Rectangle(1, 1, width - 1, height - 1));
+
+            IntPtr rColCacheIP = Memory.Allocate(4 * (ulong)roi.Width * (ulong)sizeof(double));
+            double* rColCache = (double*)rColCacheIP.ToPointer();
+
+            // Precompute and then cache the value of R() for each column
+            for (int dstX = roi.Left; dstX < roi.Right; ++dstX)
+            {
+                double srcColumn = (double)(dstX * (source.width - 1)) / (double)(width - 1);
+                double srcColumnFloor = Math.Floor(srcColumn);
+                double srcColumnFrac = srcColumn - srcColumnFloor;
+                int srcColumnInt = (int)srcColumn;
+
+                for (int m = -1; m <= 2; ++m)
+                {
+                    int index = (m + 1) + ((dstX - roi.Left) * 4);
+                    double x = m - srcColumnFrac;
+                    rColCache[index] = R(x);
+                }
+            }
+
+            // Set this up so we can cache the R()'s for every row
+            double* rRowCache = stackalloc double[4];
+
+            for (int dstY = roi.Top; dstY < roi.Bottom; ++dstY)
+            {
+                double srcRow = (double)(dstY * (source.height - 1)) / (double)(height - 1);
+                double srcRowFloor = (double)Math.Floor(srcRow);
+                double srcRowFrac = srcRow - srcRowFloor;
+                int srcRowInt = (int)srcRow;
+                ColorBgra* dstPtr = this.GetPointAddressUnchecked(roi.Left, dstY);
+
+                // Compute the R() values for this row
+                for (int n = -1; n <= 2; ++n)
+                {
+                    double x = srcRowFrac - n;
+                    rRowCache[n + 1] = R(x);
+                }
+
+                // See Perf Note below
+                //int nFirst = Math.Max(-srcRowInt, -1);
+                //int nLast = Math.Min(source.height - srcRowInt - 1, 2);
+
+                for (int dstX = roi.Left; dstX < roi.Right; dstX++)
+                {
+                    double srcColumn = (double)(dstX * (source.width - 1)) / (double)(width - 1);
+                    double srcColumnFloor = Math.Floor(srcColumn);
+                    double srcColumnFrac = srcColumn - srcColumnFloor;
+                    int srcColumnInt = (int)srcColumn;
+
+                    double blueSum = 0;
+                    double greenSum = 0;
+                    double redSum = 0;
+                    double alphaSum = 0;
+                    double totalWeight = 0;
+
+                    // See Perf Note below
+                    //int mFirst = Math.Max(-srcColumnInt, -1);
+                    //int mLast = Math.Min(source.width - srcColumnInt - 1, 2);
+
+                    ColorBgra* srcPtr = source.GetPointAddressUnchecked(srcColumnInt - 1, srcRowInt - 1);
+
+                    for (int n = -1; n <= 2; ++n)
+                    {
+                        int srcY = srcRowInt + n;
+
+                        for (int m = -1; m <= 2; ++m)
+                        {
+                            // Perf Note: It actually benchmarks faster on my system to do
+                            // a bounds check for every (m,n) than it is to limit the loop
+                            // to nFirst-Last and mFirst-mLast.
+                            // I'm leaving the code above, albeit commented out, so that
+                            // benchmarking between these two can still be performed.
+                            if (source.IsVisible(srcColumnInt + m, srcY))
+                            {
+                                double w0 = rColCache[(m + 1) + (4 * (dstX - roi.Left))];
+                                double w1 = rRowCache[n + 1];
+                                double w = w0 * w1;
+
+                                blueSum += srcPtr->B * w * srcPtr->A;
+                                greenSum += srcPtr->G * w * srcPtr->A;
+                                redSum += srcPtr->R * w * srcPtr->A;
+                                alphaSum += srcPtr->A * w;
+
+                                totalWeight += w;
+                            }
+
+                            ++srcPtr;
+                        }
+
+                        srcPtr = (ColorBgra*)((byte*)(srcPtr - 4) + source.stride);
+                    }
+
+                    double alpha = alphaSum / totalWeight;
+                    double blue;
+                    double green;
+                    double red;
+
+                    if (alpha == 0)
+                    {
+                        blue = 0;
+                        green = 0;
+                        red = 0;
+                    }
+                    else
+                    {
+                        blue = blueSum / alphaSum;
+                        green = greenSum / alphaSum;
+                        red = redSum / alphaSum;
+
+                        // add 0.5 to ensure truncation to uint results in rounding
+                        alpha += 0.5;
+                        blue += 0.5;
+                        green += 0.5;
+                        red += 0.5;
+                    }
+
+                    dstPtr->Bgra = (uint)blue + ((uint)green << 8) + ((uint)red << 16) + ((uint)alpha << 24);
+                    ++dstPtr;
+                } // for (dstX...
+            } // for (dstY...
+
+            Memory.Free(rColCacheIP);
+        }
+
+        /// <summary>
+        /// Implements bicubic filtering with NO bounds checking at any pixel.
+        /// </summary>
+        private unsafe void BicubicFitSurfaceUnchecked(Surface source, Rectangle dstRoi)
+        {
+            Rectangle roi = Rectangle.Intersect(dstRoi, this.Bounds);
+            Rectangle roiIn = Rectangle.Intersect(dstRoi, new Rectangle(1, 1, width - 1, height - 1));
+
+            IntPtr rColCacheIP = Memory.Allocate(4 * (ulong)roi.Width * (ulong)sizeof(double));
+            double* rColCache = (double*)rColCacheIP.ToPointer();
+
+            // Precompute and then cache the value of R() for each column
+            for (int dstX = roi.Left; dstX < roi.Right; ++dstX)
+            {
+                double srcColumn = (double)(dstX * (source.width - 1)) / (double)(width - 1);
+                double srcColumnFloor = Math.Floor(srcColumn);
+                double srcColumnFrac = srcColumn - srcColumnFloor;
+                int srcColumnInt = (int)srcColumn;
+
+                for (int m = -1; m <= 2; ++m)
+                {
+                    int index = (m + 1) + ((dstX - roi.Left) * 4);
+                    double x = m - srcColumnFrac;
+                    rColCache[index] = R(x);
+                }
+            }
+
+            // Set this up so we can cache the R()'s for every row
+            double* rRowCache = stackalloc double[4];
+
+            for (int dstY = roi.Top; dstY < roi.Bottom; ++dstY)
+            {
+                double srcRow = (double)(dstY * (source.height - 1)) / (double)(height - 1);
+                double srcRowFloor = Math.Floor(srcRow);
+                double srcRowFrac = srcRow - srcRowFloor;
+                int srcRowInt = (int)srcRow;
+                ColorBgra* dstPtr = this.GetPointAddressUnchecked(roi.Left, dstY);
+
+                // Compute the R() values for this row
+                for (int n = -1; n <= 2; ++n)
+                {
+                    double x = srcRowFrac - n;
+                    rRowCache[n + 1] = R(x);
+                }
+
+                rColCache = (double*)rColCacheIP.ToPointer();
+                ColorBgra* srcRowPtr = source.GetRowAddressUnchecked(srcRowInt - 1);
+
+                for (int dstX = roi.Left; dstX < roi.Right; dstX++)
+                {
+                    double srcColumn = (double)(dstX * (source.width - 1)) / (double)(width - 1);
+                    double srcColumnFloor = Math.Floor(srcColumn);
+                    double srcColumnFrac = srcColumn - srcColumnFloor;
+                    int srcColumnInt = (int)srcColumn;
+
+                    double blueSum = 0;
+                    double greenSum = 0;
+                    double redSum = 0;
+                    double alphaSum = 0;
+                    double totalWeight = 0;
+
+                    ColorBgra* srcPtr = srcRowPtr + srcColumnInt - 1;
+                    for (int n = 0; n <= 3; ++n)
+                    {
+                        double w0 = rColCache[0] * rRowCache[n];
+                        double w1 = rColCache[1] * rRowCache[n];
+                        double w2 = rColCache[2] * rRowCache[n];
+                        double w3 = rColCache[3] * rRowCache[n];
+
+                        double a0 = srcPtr[0].A;
+                        double a1 = srcPtr[1].A;
+                        double a2 = srcPtr[2].A;
+                        double a3 = srcPtr[3].A;
+
+                        alphaSum += (a0 * w0) + (a1 * w1) + (a2 * w2) + (a3 * w3);
+                        totalWeight += w0 + w1 + w2 + w3;
+
+                        blueSum += (a0 * srcPtr[0].B * w0) + (a1 * srcPtr[1].B * w1) + (a2 * srcPtr[2].B * w2) + (a3 * srcPtr[3].B * w3);
+                        greenSum += (a0 * srcPtr[0].G * w0) + (a1 * srcPtr[1].G * w1) + (a2 * srcPtr[2].G * w2) + (a3 * srcPtr[3].G * w3);
+                        redSum += (a0 * srcPtr[0].R * w0) + (a1 * srcPtr[1].R * w1) + (a2 * srcPtr[2].R * w2) + (a3 * srcPtr[3].R * w3);
+
+                        srcPtr = (ColorBgra*)((byte*)srcPtr + source.stride);
+                    }
+
+                    double alpha = alphaSum / totalWeight;
+
+                    double blue;
+                    double green;
+                    double red;
+
+                    if (alpha == 0)
+                    {
+                        blue = 0;
+                        green = 0;
+                        red = 0;
+                    }
+                    else
+                    {
+                        blue = blueSum / alphaSum;
+                        green = greenSum / alphaSum;
+                        red = redSum / alphaSum;
+
+                        // add 0.5 to ensure truncation to uint results in rounding
+                        alpha += 0.5;
+                        blue += 0.5;
+                        green += 0.5;
+                        red += 0.5;
+                    }
+
+                    dstPtr->Bgra = (uint)blue + ((uint)green << 8) + ((uint)red << 16) + ((uint)alpha << 24);
+                    ++dstPtr;
+                    rColCache += 4;
+                } // for (dstX...
+            } // for (dstY...
+
+            Memory.Free(rColCacheIP);
+        }
+
+        public unsafe void SuperSampleFitSurface(Surface source)
         {
             Rectangle dstRoi2 = Rectangle.Intersect(source.Bounds, this.Bounds);
 
@@ -354,6 +671,17 @@ namespace PaintDotNet
                     p++;
                 }
             }
+        }
+
+        /// <summary>
+        /// Determines if the requested pixel coordinate is within bounds.
+        /// </summary>
+        /// <param name="x">The x coordinate.</param>
+        /// <param name="y">The y coordinate.</param>
+        /// <returns>true if (x,y) is in bounds, false if it's not.</returns>
+        public bool IsVisible(int x, int y)
+        {
+            return x >= 0 && x < width && y >= 0 && y < height;
         }
 
         public unsafe Bitmap CreateAliasedBitmap()
