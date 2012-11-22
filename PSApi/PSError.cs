946b316baf80085186b0db862b4b5035c0bee967
0xC0000054 <0xC0000054@users.noreply.github.com>
2012-11-22 21:34:52 +0000

    Added the channel port error codes.

--- PSApi/PSError.cs
+++ PSApi/PSError.cs
@@ -13,51 +13,69 @@ namespace PSFilterLoad.PSApi
     static class PSError
     {
         /// noErr -> 0
-        public const int noErr = 0;
+        public const short noErr = 0;
         /// userCanceledErr -> (-128)
-        public const int userCanceledErr = -128;
+        public const short userCanceledErr = -128;
         /// coercedParamErr -> 2
-        public const int coercedParamErr = 2;
+        public const short coercedParamErr = 2;
         /// readErr -> (-19)
-        public const int readErr = -19;
+        public const short readErr = -19;
         /// writErr -> (-20)
-        public const int writErr = -20;
+        public const short writErr = -20;
         /// openErr -> (-23)
-        public const int openErr = -23;
+        public const short openErr = -23;
         /// dskFulErr -> (-34)
-        public const int dskFulErr = -34;
+        public const short dskFulErr = -34;
         /// ioErr -> (-36)
-        public const int ioErr = -36;
+        public const short ioErr = -36;
         /// eofErr -> (-39)
-        public const int eofErr = -39;
+        public const short eofErr = -39;
         /// fnfErr -> (-43)
-        public const int fnfErr = -43;
+        public const short fnfErr = -43;
         /// vLckdErr -> (-46)
-        public const int vLckdErr = -46;
+        public const short vLckdErr = -46;
         /// fLckdErr -> (-45)
-        public const int fLckdErr = -45;
+        public const short fLckdErr = -45;
         /// paramErr -> (-50)
-        public const int paramErr = -50;
+        public const short paramErr = -50;
         /// memFullErr -> (-108)
-        public const int memFullErr = -108;
+        public const short memFullErr = -108;
         /// nilHandleErr -> (-109)
-        public const int nilHandleErr = -109;
+        public const short nilHandleErr = -109;
         /// memWZErr -> (-111)
-        public const int memWZErr = -111;
+        public const short memWZErr = -111;
         /// errPlugInHostInsufficient -> -30900
-        public const int errPlugInHostInsufficient = -30900;
+        public const short errPlugInHostInsufficient = -30900;
         /// errPlugInPropertyUndefined -> -30901
-        public const int errPlugInPropertyUndefined = -30901;
+        public const short errPlugInPropertyUndefined = -30901;
         /// errHostDoesNotSupportColStep -> -30902
-        public const int errHostDoesNotSupportColStep = -30902;
+        public const short errHostDoesNotSupportColStep = -30902;
         /// errInvalidSamplePoint -> -30903
-        public const int errInvalidSamplePoint = -30903;
+        public const short errInvalidSamplePoint = -30903;
         /// errReportString -> -30904
-        public const int errReportString = -30904;
+        public const short errReportString = -30904;
         /// filterBadParameters -> -30100
-        public const int filterBadParameters = -30100;
+        public const short filterBadParameters = -30100;
         /// filterBadMode -> -30101
-        public const int filterBadMode = -30101;
+        public const short filterBadMode = -30101;
+
+        /// errUnknownPort -> -30910
+        public const short errUnknownPort = -30910;
+
+        /// errUnsupportedRowBits -> -30911
+        public const short errUnsupportedRowBits = -30911;
+
+        /// errUnsupportedColBits -> -30912
+        public const short errUnsupportedColBits = -30912;
+
+        /// errUnsupportedBitOffset -> -30913
+        public const short errUnsupportedBitOffset = -30913;
+
+        /// errUnsupportedDepth -> -30914
+        public const short errUnsupportedDepth = -30914;
+
+        /// errUnsupportedDepthConversion -> -30915
+        public const short errUnsupportedDepthConversion = -30915;
     }
 
 }
