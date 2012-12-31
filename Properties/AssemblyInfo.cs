b7899313c539f49a7efc543038930ae2ed9df061
0xC0000054 <0xC0000054@users.noreply.github.com>
2012-12-31 01:50:15 +0000

    Added the NeutralLanguageResource attribute.

--- Properties/AssemblyInfo.cs
+++ Properties/AssemblyInfo.cs
@@ -1,5 +1,6 @@
 ﻿using System.Reflection;
 using System.Runtime.InteropServices;
+using System.Resources;
 
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
@@ -31,3 +32,4 @@ using System.Runtime.InteropServices;
 //
 [assembly: AssemblyVersion("1.0.4.5")]
 [assembly: AssemblyFileVersion("1.0.4.5")]
+[assembly: NeutralResourcesLanguageAttribute("en-US")]
