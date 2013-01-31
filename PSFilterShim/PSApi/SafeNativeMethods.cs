5e46e916596407e8887bacdb949cb055cdf860f9
0xC0000054 <0xC0000054@users.noreply.github.com>
2013-01-31 04:05:01 +0000

    Added the Global memory functions.

--- PSFilterShim/PSApi/SafeNativeMethods.cs
+++ PSFilterShim/PSApi/SafeNativeMethods.cs
@@ -29,5 +29,24 @@ namespace PSFilterLoad.PSApi
 
         [DllImport("msvcrt.dll", CallingConvention = CallingConvention.Cdecl, SetLastError = false)]
         internal static extern IntPtr memset(IntPtr dest, int c, UIntPtr count);
+
+        [DllImport("kernel32.dll", EntryPoint = "GlobalAlloc")]
+        internal static extern IntPtr GlobalAlloc(uint uFlags, UIntPtr dwBytes);
+
+        [DllImport("kernel32.dll", EntryPoint = "GlobalSize")]
+        internal static extern IntPtr GlobalSize([In()] System.IntPtr hMem);
+
+        [DllImport("kernel32.dll", EntryPoint = "GlobalFree")]
+        internal static extern IntPtr GlobalFree([In()] System.IntPtr hMem);
+
+        [DllImport("kernel32.dll", EntryPoint = "GlobalLock")]
+        internal static extern IntPtr GlobalLock([In()] System.IntPtr hMem);
+
+        [DllImport("kernel32.dll", EntryPoint = "GlobalReAlloc")]
+        internal static extern IntPtr GlobalReAlloc([In()] IntPtr hMem, UIntPtr dwBytes, uint uFlags);
+
+        [DllImport("kernel32.dll", EntryPoint = "GlobalUnlock")]
+        [return: MarshalAs(UnmanagedType.Bool)]
+        internal static extern bool GlobalUnlock([In()] System.IntPtr hMem);
     }
 }
