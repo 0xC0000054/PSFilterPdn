cd4ce5e4dc09c22b1a95b0857a33790a699c8b45
0xC0000054 <0xC0000054@users.noreply.github.com>
2013-01-31 03:54:17 +0000

    Initial commit.

---
+++ PSApi/UnsafeNativeMethods.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Runtime.InteropServices;
+using System.Runtime.ConstrainedExecution;
+
+namespace PSFilterLoad.PSApi
+{
+    [System.Security.SuppressUnmanagedCodeSecurity]
+    internal static class UnsafeNativeMethods
+    {
+        internal delegate bool EnumResNameDelegate(IntPtr hModule, IntPtr lpszType, IntPtr lpszName, IntPtr lParam);
+
+        [DllImport("kernel32.dll", CharSet = CharSet.Unicode)]
+        internal static extern SafeLibraryHandle LoadLibraryEx(string lpFileName, IntPtr hFile, uint dwFlags);
+
+        [DllImport("kernel32.dll", EntryPoint = "EnumResourceNamesW", CharSet = CharSet.Unicode, SetLastError = true)]
+        [return: MarshalAs(UnmanagedType.Bool)]
+        internal static extern bool EnumResourceNamesW([In]IntPtr hModule, [In()]string lpszType, EnumResNameDelegate lpEnumFunc, IntPtr lParam);
+
+        [DllImport("kernel32.dll", CharSet = CharSet.Unicode)]
+        internal static extern IntPtr FindResource([In]IntPtr hModule, [In()]IntPtr lpName, [In()]IntPtr lpType);
+
+        [DllImport("kernel32.dll", EntryPoint = "LoadResource", SetLastError = true, CharSet = CharSet.Unicode)]
+        internal static extern IntPtr LoadResource(IntPtr hModule, IntPtr hResource);
+
+        [DllImport("kernel32.dll", EntryPoint = "LockResource", CharSet = CharSet.Unicode)]
+        internal static extern IntPtr LockResource(IntPtr hGlobal);
+
+        [DllImport("kernel32.dll", SetLastError = true)]
+        [return: MarshalAs(UnmanagedType.Bool)]
+        internal static extern bool FreeLibrary(IntPtr hModule);
+
+        [ReliabilityContract(Consistency.WillNotCorruptState, Cer.Success)]
+        [DllImport("kernel32.dll", CharSet = CharSet.Ansi, ExactSpelling = true, BestFitMapping = false, SetLastError = true)]
+        internal static extern IntPtr GetProcAddress([In()] SafeLibraryHandle hModule, [In(), MarshalAs(UnmanagedType.LPStr)] string lpProcName);
+    }
+}
