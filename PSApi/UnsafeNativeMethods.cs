6f6f267d40b43f052705bb5829bc436a78282e53
0xC0000054 <0xC0000054@users.noreply.github.com>
2013-02-15 03:31:41 +0000

    Use ExactSpelling on the Unicode imports.

--- PSApi/UnsafeNativeMethods.cs
+++ PSApi/UnsafeNativeMethods.cs
@@ -7,27 +7,27 @@ namespace PSFilterLoad.PSApi
     [System.Security.SuppressUnmanagedCodeSecurity]
     internal static class UnsafeNativeMethods
     {
-        internal delegate bool EnumResNameDelegate(IntPtr hModule, IntPtr lpszType, IntPtr lpszName, IntPtr lParam);
+        internal delegate bool EnumResNameDelegate([In()] IntPtr hModule, [In()] IntPtr lpszType, [In()] IntPtr lpszName, [In()] IntPtr lParam);
 
-        [DllImport("kernel32.dll", CharSet = CharSet.Unicode)]
-        internal static extern SafeLibraryHandle LoadLibraryEx(string lpFileName, IntPtr hFile, uint dwFlags);
+        [DllImport("kernel32.dll", CharSet = CharSet.Unicode, ExactSpelling = true)]
+        internal static extern SafeLibraryHandle LoadLibraryExW([In()] string lpFileName, IntPtr hFile, uint dwFlags);
 
-        [DllImport("kernel32.dll", EntryPoint = "EnumResourceNamesW", CharSet = CharSet.Unicode, SetLastError = true)]
+        [DllImport("kernel32.dll", CharSet = CharSet.Unicode, ExactSpelling = true)]
         [return: MarshalAs(UnmanagedType.Bool)]
-        internal static extern bool EnumResourceNamesW([In]IntPtr hModule, [In()]string lpszType, EnumResNameDelegate lpEnumFunc, IntPtr lParam);
+        internal static extern bool EnumResourceNamesW([In()] IntPtr hModule, [In()] string lpszType, EnumResNameDelegate lpEnumFunc, [In()] IntPtr lParam);
 
-        [DllImport("kernel32.dll", CharSet = CharSet.Unicode)]
-        internal static extern IntPtr FindResource([In]IntPtr hModule, [In()]IntPtr lpName, [In()]IntPtr lpType);
+        [DllImport("kernel32.dll", CharSet = CharSet.Unicode, ExactSpelling = true)]
+        internal static extern IntPtr FindResourceW([In()] IntPtr hModule, [In()] IntPtr lpName, [In()] IntPtr lpType);
 
-        [DllImport("kernel32.dll", EntryPoint = "LoadResource", SetLastError = true, CharSet = CharSet.Unicode)]
-        internal static extern IntPtr LoadResource(IntPtr hModule, IntPtr hResource);
+        [DllImport("kernel32.dll", EntryPoint = "LoadResource")]
+        internal static extern IntPtr LoadResource([In()] IntPtr hModule, [In()] IntPtr hResource);
 
-        [DllImport("kernel32.dll", EntryPoint = "LockResource", CharSet = CharSet.Unicode)]
-        internal static extern IntPtr LockResource(IntPtr hGlobal);
+        [DllImport("kernel32.dll", EntryPoint = "LockResource")]
+        internal static extern IntPtr LockResource([In()] IntPtr hGlobal);
 
-        [DllImport("kernel32.dll", SetLastError = true)]
+        [DllImport("kernel32.dll", EntryPoint = "FreeLibrary")]
         [return: MarshalAs(UnmanagedType.Bool)]
-        internal static extern bool FreeLibrary(IntPtr hModule);
+        internal static extern bool FreeLibrary([In()] IntPtr hModule);
 
         [ReliabilityContract(Consistency.WillNotCorruptState, Cer.Success)]
         [DllImport("kernel32.dll", CharSet = CharSet.Ansi, ExactSpelling = true, BestFitMapping = false, SetLastError = true)]
