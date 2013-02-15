e602328ca530b26172def550a55b9db165eb8187
0xC0000054 <0xC0000054@users.noreply.github.com>
2013-02-15 04:19:27 +0000

    Enable SetLastError on LoadLibraryW.

--- PSFilterShim/PSApi/UnsafeNativeMethods.cs
+++ PSFilterShim/PSApi/UnsafeNativeMethods.cs
@@ -6,14 +6,14 @@ namespace PSFilterLoad.PSApi
     [System.Security.SuppressUnmanagedCodeSecurity]
     internal static class UnsafeNativeMethods
     {
-        [DllImport("kernel32.dll", CharSet = CharSet.Unicode)]
-        public static extern SafeLibraryHandle LoadLibrary(string lpFileName);
+        [DllImport("kernel32.dll", CharSet = CharSet.Unicode, ExactSpelling = true, SetLastError = true)]
+        public static extern SafeLibraryHandle LoadLibraryW(string lpFileName);
 
-        [DllImport("kernel32.dll", SetLastError = true)]
+        [DllImport("kernel32.dll", ExactSpelling = true)]
         [return: MarshalAs(UnmanagedType.Bool)]
         public static extern bool FreeLibrary(IntPtr hModule);
 
-        [DllImport("kernel32", CharSet = CharSet.Ansi, ExactSpelling = true, SetLastError = true, BestFitMapping = false)]
+        [DllImport("kernel32", CharSet = CharSet.Ansi, ExactSpelling = true, BestFitMapping = false)]
         public static extern IntPtr GetProcAddress([In()] SafeLibraryHandle hModule, [In(), MarshalAs(UnmanagedType.LPStr)] string lpProcName);
     }
 }
