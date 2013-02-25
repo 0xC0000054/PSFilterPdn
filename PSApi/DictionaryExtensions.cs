c2899b2506d05c3e2a41b31851b607d7621169b4
0xC0000054 <0xC0000054@users.noreply.github.com>
2013-02-25 21:21:59 +0000

    Corrected spelling.

---
+++ PSApi/DictionaryExtensions.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace PSFilterLoad.PSApi
+{
+    public static class DictionaryExtensions
+    {
+        public static void AddOrUpdate<TKey, TValue>(this IDictionary<TKey, TValue> dictionary, TKey key, TValue value)
+        {
+            if (!dictionary.ContainsKey(key))
+            {
+                dictionary.Add(key, value);
+            }
+            else
+            {
+                dictionary[key] = value;
+            }
+        }
+    }
+}
