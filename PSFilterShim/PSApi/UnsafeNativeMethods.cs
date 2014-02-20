bb5a1d52e82d0e266dda6e59da1042e3ffd64af3
0xC0000054 <0xC0000054@users.noreply.github.com>
2014-02-20 01:09:38 +0000

    Mark methods as internal.

--- PSFilterShim/PSApi/UnsafeNativeMethods.cs
+++ PSFilterShim/PSApi/UnsafeNativeMethods.cs
@@ -7,13 +7,13 @@ namespace PSFilterLoad.PSApi
     internal static class UnsafeNativeMethods
     {
         [DllImport("kernel32.dll", CharSet = CharSet.Unicode, ExactSpelling = true, SetLastError = true)]
-        public static extern SafeLibraryHandle LoadLibraryW(string lpFileName);
+        internal static extern SafeLibraryHandle LoadLibraryW(string lpFileName);
 
         [DllImport("kernel32.dll", ExactSpelling = true)]
         [return: MarshalAs(UnmanagedType.Bool)]
-        public static extern bool FreeLibrary(IntPtr hModule);
+        internal static extern bool FreeLibrary(IntPtr hModule);
 
         [DllImport("kernel32", CharSet = CharSet.Ansi, ExactSpelling = true, BestFitMapping = false)]
-        public static extern IntPtr GetProcAddress([In()] SafeLibraryHandle hModule, [In(), MarshalAs(UnmanagedType.LPStr)] string lpProcName);
+        internal static extern IntPtr GetProcAddress([In()] SafeLibraryHandle hModule, [In(), MarshalAs(UnmanagedType.LPStr)] string lpProcName);
     }
 }
