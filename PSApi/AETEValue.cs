80ee2d44538e7b84c035eb2e12750adccd843b24
0xC0000054 <0xC0000054@users.noreply.github.com>
2014-02-20 00:44:43 +0000

    Added an UnitFloat struct.

--- PSApi/AETEValue.cs
+++ PSApi/AETEValue.cs
@@ -2,53 +2,82 @@
 
 namespace PSFilterLoad.PSApi
 {
-    [Serializable]
-	public struct AETEValue
-    {
-        private uint type;
-        private int flags;
-        private int size;
-        private object value;
+	[Serializable]
+	public sealed class AETEValue
+	{
+		private uint type;
+		private int flags;
+		private int size;
+		private object value;
 
-        public uint Type
-        {
+		public uint Type
+		{
+			get
+			{
+				return type;
+			}
+		}
+
+		public int Flags
+		{
+			get
+			{
+				return flags;
+			}
+		}
+
+		public int Size
+		{
+			get
+			{
+				return size;
+			}
+		}
+
+		public object Value
+		{
+			get
+			{
+				return value;
+			}
+		}
+
+		public AETEValue(uint type, int flags, int size, object value)
+		{
+			this.type = type;
+			this.flags = flags;
+			this.size = size;
+			this.value = value;
+		}
+	}
+
+	[Serializable]
+	public struct UnitFloat
+	{
+        private uint unit;
+        private double value;
+
+		public uint Unit
+		{
             get
             {
-                return type;
+                return unit;
             }
-        }
+		}
 
-        public int Flags
-        {
-            get
-            {
-                return flags;
-            }
-        }
-
-        public int Size
-        {
-            get
-            {
-                return size;
-            }
-        }
-
-        public object Value
-        {
+		public double Value
+		{
             get
             {
                 return value;
             }
-        }
+		}
 
-        public AETEValue(uint type, int flags, int size, object value)
-        {
-            this.type = type;
-            this.flags = flags;
-            this.size = size;
-            this.value = value;
-        }
-    } 
+		public UnitFloat(uint unit, double value)
+		{
+			this.unit = unit;
+			this.value = value;
+		}
+	}
   
 }
