3a3e47855d0b1a64015feb735182f44a09609fc9
0xC0000054 <0xC0000054@users.noreply.github.com>
2014-04-09 01:48:05 +0000

    Added the PropertySuite and ResourceSuite flags.

--- PSFilterShim/PSApi/DebugFlags.cs
+++ PSFilterShim/PSApi/DebugFlags.cs
@@ -7,17 +7,20 @@ namespace PSFilterLoad.PSApi
     enum DebugFlags
     {
         None = 0,
-        AdvanceState = 1,
-        BufferSuite = 2,
-        Call = 4,
-        ChannelPorts = 8,
-        ColorServices = 16,
-        DescriptorParameters = 32,
-        DisplayPixels = 64,
-        Error = 128,
-        HandleSuite = 256,
-        MiscCallbacks = 512,
-        PiPL = 1024
+        AdvanceState = (1 << 0),
+        BufferSuite = (1 << 1),
+        Call = (1 << 2),
+        ChannelPorts = (1 << 3),
+        ColorServices = (1 << 4),
+        DescriptorParameters = (1 << 5),
+        DisplayPixels = (1 << 6),
+        Error = (1 << 7),
+        HandleSuite = (1 << 8),
+        ImageServices = (1 << 9),
+        MiscCallbacks = (1 << 10),
+        PiPL = (1 << 11),
+        PropertySuite = (1 << 12),
+        ResourceSuite = (1 << 13)
     }
 #endif
 }
