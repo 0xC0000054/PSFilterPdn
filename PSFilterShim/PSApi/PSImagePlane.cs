9ea0471c602a75854afd33b8ed7096eda948a8b8
0xC0000054 <0xC0000054@users.noreply.github.com>
2014-07-12 23:06:45 -0600

    Added the USEIMAGESERVICES preprocessor directive.

--- PSFilterShim/PSApi/PSImagePlane.cs
+++ PSFilterShim/PSApi/PSImagePlane.cs
@@ -8,22 +8,14 @@ using System.Runtime.InteropServices;
 
 namespace PSFilterLoad.PSApi
 {
-
+#if USEIMAGESERVICES
     [StructLayout(LayoutKind.Sequential)]
     internal struct PSImagePlane
     {
-
-        /// void*
         public IntPtr data;
-
-        /// Rect
         public Rect16 bounds;
-
-        /// int32->int
         public int rowBytes;
-
-        /// int32->int
         public int colBytes;
     }
-    
+#endif
 }
