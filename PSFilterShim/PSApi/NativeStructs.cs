c21108193bd35a34eb80a9f4700d4b9d7a93a58e
0xC0000054 <0xC0000054@users.noreply.github.com>
2015-03-22 22:53:18 -0600

    Added RGNDATAHEADER, RGNDATA and RECT.

--- PSFilterShim/PSApi/NativeStructs.cs
+++ PSFilterShim/PSApi/NativeStructs.cs
@@ -10,6 +10,15 @@
 //
 /////////////////////////////////////////////////////////////////////////////////
 
+/////////////////////////////////////////////////////////////////////////////////
+// Paint.NET                                                                   //
+// Copyright (C) dotPDN LLC, Rick Brewster, Tom Jackson, and contributors.     //
+// Portions Copyright (C) Microsoft Corporation. All Rights Reserved.          //
+// See src/Resources/Files/License.txt for full licensing and attribution      //
+// details.                                                                    //
+// .                                                                           //
+/////////////////////////////////////////////////////////////////////////////////
+
 using System.Runtime.InteropServices;
 
 namespace PSFilterLoad.PSApi
@@ -26,6 +35,39 @@ namespace PSFilterLoad.PSApi
             public uint State;
             public uint Protect;
             public uint Type;
+        }
+
+#pragma warning disable 0649
+        [StructLayout(LayoutKind.Sequential)]
+        internal struct RGNDATAHEADER
+        {
+            internal uint dwSize;
+            internal uint iType;
+            internal uint nCount;
+            internal uint nRgnSize;
+            internal RECT rcBound;
+        };
+
+#pragma warning restore 0649
+
+        [StructLayout(LayoutKind.Sequential)]
+        internal struct RGNDATA
+        {
+            internal RGNDATAHEADER rdh;
+
+            internal unsafe static RECT* GetRectsPointer(RGNDATA* me)
+            {
+                return (RECT*)((byte*)me + sizeof(RGNDATAHEADER));
+            }
+        }
+
+        [StructLayout(LayoutKind.Sequential)]
+        internal struct RECT
+        {
+            internal int left;
+            internal int top;
+            internal int right;
+            internal int bottom;
         }
     }
 }
