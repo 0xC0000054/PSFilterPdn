f4d1a7e7a66b0008a28477ee549338793d688549
0xC0000054 <0xC0000054@users.noreply.github.com>
2015-07-21 04:58:41 -0600

    Removed the order field.

--- TreeNodeItemComparer.cs
+++ TreeNodeItemComparer.cs
@@ -17,22 +17,13 @@ namespace PSFilterPdn
 {
     internal sealed class TreeNodeItemComparer : IComparer
     {
-        private SortOrder order;
         public TreeNodeItemComparer()
         {
-            this.order = SortOrder.Ascending;
         }
 
         public int Compare(object x, object y)
         {
-            int returnVal = -1;
-            returnVal = StringLogicalComparer.Compare(((TreeNode)x).Text,
-                                    ((TreeNode)y).Text);
-            // Determine whether the sort order is descending.
-            if (order == SortOrder.Descending)
-                // Invert the value returned by String.Compare.
-                returnVal *= -1;
-            return returnVal;
+            return StringLogicalComparer.Compare(((TreeNode)x).Text, ((TreeNode)y).Text);
         }
     }
 }
