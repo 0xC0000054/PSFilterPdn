c3f44bf093368479b2a67dfed235244b6da0d4a0
0xC0000054 <0xC0000054@users.noreply.github.com>
2016-03-30 21:19:42 -0600

    Added StringUtil.cs.

---
+++ PSApi/StringUtil.cs
@@ -0,0 +1,127 @@
+﻿/////////////////////////////////////////////////////////////////////////////////
+//
+// Photoshop-compatible filter host Effect plugin for Paint.NET
+// http://psfilterpdn.codeplex.com/
+//
+// This software is provided under the Microsoft Public License:
+//   Copyright (C) 2010-2016 Nicholas Hayes
+// 
+// See LICENSE.txt for complete licensing and attribution information.
+//
+/////////////////////////////////////////////////////////////////////////////////
+
+using System;
+using System.Runtime.InteropServices;
+using System.Text;
+
+namespace PSFilterLoad.PSApi
+{
+	internal static class StringUtil
+	{
+		private static readonly Encoding Windows1252Encoding = Encoding.GetEncoding(1252);
+		private static readonly char[] TrimChars = new char[] { ' ', '\0' };
+
+		/// <summary>
+		/// Creates a <see cref="string"/> from a Pascal string.
+		/// </summary>
+		/// <param name="pascalString">The pascal string to convert.</param>
+		/// <param name="defaultValue">The value returned if <paramref name="pascalString"/> is null.</param>
+		/// <returns>
+		/// A managed string that holds a copy of the Pascal string.
+		/// If <paramref name="pascalString"/> is null, the method returns <paramref name="defaultValue"/>.
+		/// </returns>
+		internal static unsafe string FromPascalString(IntPtr pascalString, string defaultValue)
+		{
+			if (pascalString == IntPtr.Zero)
+			{
+				return defaultValue;
+			}
+
+			return FromPascalString((byte*)pascalString.ToPointer());
+		}
+
+		/// <summary>
+		/// Creates a <see cref="string"/> from a Pascal string.
+		/// </summary>
+		/// <param name="pascalString">The pascal string to convert.</param>
+		/// <returns>
+		/// A managed string that holds a copy of the Pascal string.
+		/// If <paramref name="pascalString"/> is null, the method returns null.
+		/// </returns>
+		internal static unsafe string FromPascalString(byte* pascalString)
+		{
+			if (pascalString == null)
+			{
+				return null;
+			}
+
+			int length = pascalString[0];
+
+			return new string((sbyte*)pascalString, 1, length, Windows1252Encoding).Trim(TrimChars);
+		}
+
+		/// <summary>
+		/// Creates a <see cref="string"/> from a Pascal string.
+		/// </summary>
+		/// <param name="pascalString">The pascal string to convert.</param>
+		/// <param name="lengthWithPrefix">The length of the resulting string including the length prefix.</param>
+		/// <returns>
+		/// A managed string that holds a copy of the Pascal string.
+		/// If <paramref name="pascalString"/> is null, the method returns null.
+		/// </returns>
+		internal static unsafe string FromPascalString(byte* pascalString, out int lengthWithPrefix)
+		{
+			if (pascalString == null)
+			{
+				lengthWithPrefix = 0;
+				return null;
+			}
+
+			// Include the length prefix byte in the total.
+			lengthWithPrefix = pascalString[0] + 1;
+
+			return new string((sbyte*)pascalString, 1, pascalString[0], Windows1252Encoding);
+		}
+
+		/// <summary>
+		/// Creates a <see cref="string"/> from a C string.
+		/// </summary>
+		/// <param name="ptr">The pointer to read from.</param>
+		/// <returns>
+		/// A managed string that holds a copy of the C string.
+		/// </returns>
+		internal static string FromCString(IntPtr ptr)
+		{
+			string data = Marshal.PtrToStringAnsi(ptr);
+			if (data == null)
+			{
+				return null;
+			}
+
+			return data.Trim(TrimChars);
+		}
+
+		/// <summary>
+		/// Creates a <see cref="string"/> from a C string.
+		/// </summary>
+		/// <param name="ptr">The pointer to read from.</param>
+		/// <param name="lengthWithTerminator">The length of the resulting string including the NUL terminator.</param>
+		/// <returns>
+		/// A managed string that holds a copy of the C string.
+		/// </returns>
+		internal static string FromCString(IntPtr ptr, out int lengthWithTerminator)
+		{
+			string data = Marshal.PtrToStringAnsi(ptr);
+			if (data == null)
+			{
+				lengthWithTerminator = 0;
+				return null;
+			}
+
+			// Add the terminating NUL to the total length.
+			lengthWithTerminator = data.Length + 1;
+
+			return data.Trim(TrimChars);
+		}
+	}
+}
---
+++ PSFilterShim/PSApi/StringUtil.cs
@@ -0,0 +1,46 @@
+﻿/////////////////////////////////////////////////////////////////////////////////
+//
+// Photoshop-compatible filter host Effect plugin for Paint.NET
+// http://psfilterpdn.codeplex.com/
+//
+// This software is provided under the Microsoft Public License:
+//   Copyright (C) 2010-2016 Nicholas Hayes
+// 
+// See LICENSE.txt for complete licensing and attribution information.
+//
+/////////////////////////////////////////////////////////////////////////////////
+
+using System;
+using System.Text;
+
+namespace PSFilterLoad.PSApi
+{
+	internal static class StringUtil
+	{
+		private static readonly Encoding Windows1252Encoding = Encoding.GetEncoding(1252);
+		private static readonly char[] TrimChars = new char[] { ' ', '\0' };
+
+		/// <summary>
+		/// Creates a <see cref="string"/> from a Pascal string.
+		/// </summary>
+		/// <param name="pascalString">The pascal string to convert.</param>
+		/// <param name="defaultValue">The value returned if <paramref name="pascalString"/> is null.</param>
+		/// <returns>
+		/// A managed string that holds a copy of the Pascal string.
+		/// If <paramref name="pascalString"/> is null, the method returns <paramref name="defaultValue"/>.
+		/// </returns>
+		internal static unsafe string FromPascalString(IntPtr pascalString, string defaultValue)
+		{
+			if (pascalString == IntPtr.Zero)
+			{
+				return defaultValue;
+			}
+
+			byte* ptr = (byte*)pascalString.ToPointer();
+
+			int length = ptr[0];
+
+			return new string((sbyte*)ptr, 1, length, Windows1252Encoding).Trim(TrimChars);
+		}
+	}
+}
