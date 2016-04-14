5c43eec14d42087bba178bdfc984415d3ba039ad
0xC0000054 <0xC0000054@users.noreply.github.com>
2016-04-14 04:56:55 -0600

    Added StringExtensions.cs.

---
+++ StringExtensions.cs
@@ -0,0 +1,24 @@
+﻿/////////////////////////////////////////////////////////////////////////////////
+//
+// Photoshop-compatible filter host Effect plugin for Paint.NET
+// http://psfilterpdn.codeplex.com/
+//
+// This software is provided under the Microsoft Public License:
+//   Copyright (C) 2010-2016 Nicholas Hayes
+// 
+// See LICENSE.txt for complete licensing and attribution information.
+//
+/////////////////////////////////////////////////////////////////////////////////
+
+using System;
+
+namespace PSFilterPdn
+{
+    internal static class StringExtensions
+    {
+        internal static bool Contains(this string s, string value, StringComparison comparisonType)
+        {
+            return (s.IndexOf(value, comparisonType) >= 0);
+        }
+    }
+}
