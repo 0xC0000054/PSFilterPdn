5fd1f3a7db2b72f2504f7a7f629e252ab84424a9
0xC0000054 <0xC0000054@users.noreply.github.com>
2016-05-28 21:33:04 -0600

    Added PSFilterShimServer.cs.

---
+++ PSFilterShimServer.cs
@@ -0,0 +1,54 @@
+﻿/////////////////////////////////////////////////////////////////////////////////
+//
+// Photoshop-compatible filter host Effect plugin for Paint.NET
+// http://psfilterpdn.codeplex.com/
+//
+// This software is provided under the Microsoft Public License:
+//   Copyright (C) 2010-2016 Nicholas Hayes
+// 
+// See LICENSE.txt for complete licensing and attribution information.
+//
+/////////////////////////////////////////////////////////////////////////////////
+
+using System;
+using System.ServiceModel;
+
+namespace PSFilterPdn
+{
+    // Adapted from: http://www.jmedved.com/2010/03/named-pipes-in-wcf/ 
+    internal static class PSFilterShimServer
+    {
+        private static readonly Uri ServiceUri = new Uri("net.pipe://localhost/PSFilterShim");
+        private const string PipeName = "ShimData";
+
+        internal const string EndpointName = "net.pipe://localhost/PSFilterShim/ShimData";
+
+        private static PSFilterShimService _service = null;
+        private static ServiceHost _host = null;
+
+        /// <summary>
+        /// Starts the WCF server service.
+        /// </summary>
+        /// <param name="service">The service instance to use.</param>
+        public static void Start(PSFilterShimService service)
+        {
+            _service = service;
+
+            _host = new ServiceHost(_service, ServiceUri);
+            _host.AddServiceEndpoint(typeof(IPSFilterShim), new NetNamedPipeBinding(), PipeName);
+            _host.Open();
+        }
+
+        /// <summary>
+        /// Stops the WCF server instance.
+        /// </summary>
+        public static void Stop()
+        {
+            if ((_host != null) && (_host.State != CommunicationState.Closed))
+            {
+                _host.Close();
+                _host = null;
+            }
+        }
+    }
+}
