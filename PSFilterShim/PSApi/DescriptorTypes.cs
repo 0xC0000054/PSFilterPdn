08cd111b0e52e5535c1d0b16d408b38af7bf5ad6
0xC0000054 <0xC0000054@users.noreply.github.com>
2016-10-05 16:53:22 -0600

    Corrected the definitions of the typeAlias and typePath constants.

--- PSFilterShim/PSApi/DescriptorTypes.cs
+++ PSFilterShim/PSApi/DescriptorTypes.cs
@@ -19,7 +19,7 @@ namespace PSFilterLoad.PSApi
 {
     internal static class DescriptorTypes
     {
-        public const uint typeAlias = 0x976c6973U; // 'alis'
+        public const uint typeAlias = 0x616c6973U; // 'alis'
         public const uint typeBoolean = 0x626f6f6cU; // 'bool'
         public const uint typeChar = 0x54455854U; // 'TEXT'
         public const uint typeClass = 0x74797065U; // 'type'
@@ -27,7 +27,7 @@ namespace PSFilterLoad.PSApi
         public const uint typeInteger = 0x6c6f6e67U; // 'long'
         public const uint typeNull = 0x6e756c6cU; // 'null'
         public const uint typeObjectRefrence = 0x6f626a20U; // 'obj '
-        public const uint typePath = 0x50746820U; // 'Pat '
+        public const uint typePath = 0x50617420U; // 'Pat '
         public const uint typeUintFloat = 0x556e7446; // 'UntF'
 
         public const uint classRGBColor = 0x52474243U; // 'RGBC'
