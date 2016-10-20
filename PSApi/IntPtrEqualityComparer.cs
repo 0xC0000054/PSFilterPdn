9033402375b6fb47a5c43e78bffb4ec77776a2d0
0xC0000054 <0xC0000054@users.noreply.github.com>
2016-10-20 04:58:58 -0600

    Added a summary to the class and a comment describing why it is needed.

--- PSApi/IntPtrEqualityComparer.cs
+++ PSApi/IntPtrEqualityComparer.cs
@@ -15,6 +15,14 @@ using System.Collections.Generic;
 
 namespace PSFilterLoad.PSApi
 {
+    // As IntPtr does not implement IEquatable<T> in .NET 3.5 using
+    // EqualityComparer<IntPtr>.Default will cause it to be boxed during comparisons.
+    // This comparer is used with any types that need an IEqualityComparer<IntPtr> to prevent boxing.
+
+    /// <summary>
+    /// Represents an equality comparer for <see cref="IntPtr"/>.
+    /// </summary>
+    /// <seealso cref="IEqualityComparer{T}"/>
     internal sealed class IntPtrEqualityComparer : IEqualityComparer<IntPtr>
     {
         private static readonly IntPtrEqualityComparer instance = new IntPtrEqualityComparer();
