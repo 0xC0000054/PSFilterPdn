2b0bedeb4d9bb5734b7ec23ad54ad88e4a050e5c
0xC0000054 <0xC0000054@users.noreply.github.com>
2016-10-19 20:31:29 -0600

    Added IntPtrEqualityComparer.cs.

---
+++ PSFilterShim/PSApi/IntPtrEqualityComparer.cs
@@ -0,0 +1,44 @@
+﻿/////////////////////////////////////////////////////////////////////////////////
+//
+// Photoshop-compatible filter host Effect plugin for Paint.NET
+// http://psfilterpdn.codeplex.com/
+//
+// This software is provided under the Microsoft Public License:
+//   Copyright (C) 2010-2016 Nicholas Hayes
+// 
+// See LICENSE.txt for complete licensing and attribution information.
+//
+/////////////////////////////////////////////////////////////////////////////////
+
+using System;
+using System.Collections.Generic;
+
+namespace PSFilterLoad.PSApi
+{
+    internal sealed class IntPtrEqualityComparer : IEqualityComparer<IntPtr>
+    {
+        private static readonly IntPtrEqualityComparer instance = new IntPtrEqualityComparer();
+
+        private IntPtrEqualityComparer()
+        {
+        }
+
+        public static IntPtrEqualityComparer Instance
+        {
+            get
+            {
+                return instance;
+            }
+        }
+
+        public bool Equals(IntPtr x, IntPtr y)
+        {
+            return x == y;
+        }
+
+        public int GetHashCode(IntPtr obj)
+        {
+            return obj.GetHashCode();
+        }
+    }
+}
