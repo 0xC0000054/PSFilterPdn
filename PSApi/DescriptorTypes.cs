9633eaa6d1a28bcd9cb9fd15a12dcab1df4d6944
0xC0000054 <0xC0000054@users.noreply.github.com>
2016-10-24 04:49:41 -0600

    Added a typeObject constant.

--- PSApi/DescriptorTypes.cs
+++ PSApi/DescriptorTypes.cs
@@ -27,6 +27,7 @@ namespace PSFilterLoad.PSApi
         public const uint typeGlobalClass = 0x476c6243U; // 'GlbC'
         public const uint typeInteger = 0x6c6f6e67U; // 'long'
         public const uint typeNull = 0x6e756c6cU; // 'null'
+        public const uint typeObject = 0x4f626a63U; // 'Objc'
         public const uint typeObjectRefrence = 0x6f626a20U; // 'obj '
         public const uint typePath = 0x50617420U; // 'Pat '
         public const uint typeRawData = 0x74647461U; // 'tdta'
