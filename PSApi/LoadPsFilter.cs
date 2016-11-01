1fa27e2dc7281cc18553f22ce5435a517e227104
0xC0000054 <0xC0000054@users.noreply.github.com>
2016-10-31 23:34:31 -0600

    Check that the channelReadDescPtrs field is not null in Dispose(bool) method.

--- PSApi/LoadPsFilter.cs
+++ PSApi/LoadPsFilter.cs
@@ -4193,7 +4193,7 @@ namespace PSFilterLoad.PSApi
 					readDocumentPtr = IntPtr.Zero;
 				}
 
-				if (channelReadDescPtrs.Count > 0)
+				if (channelReadDescPtrs != null)
 				{
 					foreach (var item in channelReadDescPtrs)
 					{
--- PSFilterShim/PSApi/LoadPsFilter.cs
+++ PSFilterShim/PSApi/LoadPsFilter.cs
@@ -4217,7 +4217,7 @@ namespace PSFilterLoad.PSApi
 					readDocumentPtr = IntPtr.Zero;
 				}
 
-				if (channelReadDescPtrs.Count > 0)
+				if (channelReadDescPtrs != null)
 				{
 					foreach (var item in channelReadDescPtrs)
 					{
