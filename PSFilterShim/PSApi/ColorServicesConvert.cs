e3daa5ae14a5a6b4f1bca27b3137e9fc25ff3e5e
0xC0000054 <0xC0000054@users.noreply.github.com>
2016-11-21 07:53:27 -0700

    Fixed the scaling of the output Lab and XYZ color ranges in the Convert methods.

--- PSFilterShim/PSApi/ColorServicesConvert.cs
+++ PSFilterShim/PSApi/ColorServicesConvert.cs
@@ -108,10 +108,14 @@ namespace PSFilterLoad.PSApi
                         colorComponents[2] = (short)(result.component2 * 255.0);
                         break;
                     case ColorSpace.LabSpace:
+                        colorComponents[0] = (short)((result.component0 / 116) * 255.0);
+                        colorComponents[1] = (short)((result.component1 / 500) * 255.0);
+                        colorComponents[2] = (short)((result.component2 / 200) * 255.0);
+                        break;
                     case ColorSpace.XYZSpace:
-                        colorComponents[0] = (short)(result.component0 * 255.0);
-                        colorComponents[1] = (short)(result.component1 * 255.0);
-                        colorComponents[2] = (short)(result.component2 * 255.0);
+                        colorComponents[0] = (short)((result.component0 / CIEXYZ.D65.X) * 255.0);
+                        colorComponents[1] = (short)((result.component1 / CIEXYZ.D65.Y) * 255.0);
+                        colorComponents[2] = (short)((result.component2 / CIEXYZ.D65.Z) * 255.0);
                         break;
                     case ColorSpace.CMYKSpace:
                         colorComponents[0] = (short)(result.component0 * 255.0);
@@ -228,9 +232,17 @@ namespace PSFilterLoad.PSApi
                         c1 = (byte)result.component1;
                         c2 = (byte)result.component2;
                         break;
-                    case ColorSpace.CMYKSpace:
                     case ColorSpace.LabSpace:
+                        c0 = (byte)((result.component0 / 116) * 255.0);
+                        c1 = (byte)((result.component1 / 500) * 255.0);
+                        c2 = (byte)((result.component2 / 200) * 255.0);
+                        break;
                     case ColorSpace.XYZSpace:
+                        c0 = (byte)((result.component0 / CIEXYZ.D65.X) * 255.0);
+                        c1 = (byte)((result.component1 / CIEXYZ.D65.Y) * 255.0);
+                        c2 = (byte)((result.component2 / CIEXYZ.D65.Z) * 255.0);
+                        break;
+                    case ColorSpace.CMYKSpace:
                     default:
                         c0 = (byte)(result.component0 * 255.0);
                         c1 = (byte)(result.component1 * 255.0);
