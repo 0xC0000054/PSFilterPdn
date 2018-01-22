0xC0000054/PSFilterPdn

commit d491166db39d9ee0bdb99096d3bf8b58de726052

Rename the parmData variable to filterParameters.

PSFilterShim/Program.cs
@@ -131,14 +131,14 @@ namespace PSFilterShim
 			{
 				
 
-				ParameterData parmData = null;
+				ParameterData filterParameters = null;
 				string parmDataFileName = Console.ReadLine();
 				if (!string.IsNullOrEmpty(parmDataFileName) && File.Exists(parmDataFileName))
 				{
 					using (FileStream fs = new FileStream(parmDataFileName, FileMode.Open, FileAccess.Read, FileShare.None, 4096, FileOptions.DeleteOnClose))
 					{
 						BinaryFormatter bf = new BinaryFormatter();
-						parmData = (ParameterData)bf.Deserialize(fs);
+						filterParameters = (ParameterData)bf.Deserialize(fs);
 					}
 				}
 
@@ -161,14 +161,16 @@ namespace PSFilterShim
 					}
 					lps.SetProgressCallback(progressCallback);
 
-					if (parmData != null)
+					if (filterParameters != null)
 					{
 						// ignore the filters that only use the data handle, e.g. Filter Factory  
-						if (((parmData.GlobalParameters.GetParameterDataBytes() != null && parmData.GlobalParameters.GetPluginDataBytes() != null) ||
-							(parmData.GlobalParameters.GetParameterDataBytes() != null && parmData.GlobalParameters.GetPluginDataBytes() == null)) ||
-							parmData.AETEDictionary != null)
+					    byte[] parameterData = filterParameters.GlobalParameters.GetParameterDataBytes();
+					    byte[] pluginData = filterParameters.GlobalParameters.GetPluginDataBytes();
+
+					    if ((parameterData != null && pluginData != null || parameterData != null && pluginData == null) ||
+						    filterParameters.AETEDictionary.Count > 0)
 						{
-							lps.FilterParameters = parmData;
+							lps.FilterParameters = filterParameters;
 							lps.IsRepeatEffect = repeatEffect;
 						}
 					}

commit 5eab2fc5849710465342f25bcffed4a2d82b5922

Check if the AETEDictionary property is null
Check if the AETEDictionary property is null instead of checking that
the dictionary count is greater than zero.
The AETEDictionary property can be null when a filter does not use the
scripting suites.

This fixes a regression introduced in https://github.com/0xC0000054/PSFilterPdn/commit/d491166db39d9ee0bdb99096d3bf8b58de726052

PSFilterShim/Program.cs
@@ -170,7 +170,7 @@ namespace PSFilterShim
 						// Ignore the filters that only use the data handle, e.g. Filter Factory.
 						byte[] parameterData = filterParameters.GlobalParameters.GetParameterDataBytes();
 
-						if (parameterData != null || filterParameters.AETEDictionary.Count > 0)
+						if (parameterData != null || filterParameters.AETEDictionary != null)
 						{
 							lps.FilterParameters = filterParameters;
 							lps.IsRepeatEffect = settings.RepeatEffect;