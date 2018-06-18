0xC0000054/PSFilterPdn

commit 0e46099ea8742381055013b533b302b9f5f3d4e9

Set the *Restored fields before allocating memory
Set the parameterDataRestored and pluginDataRestored fields before
allocating memory for the parameter handles.
This allows the memory to be freed when the class is disposed if an
exception is thrown when copying the data.

PSApi/LoadPsFilter.cs
@@ -729,6 +729,7 @@ namespace PSFilterLoad.PSApi
 			byte[] parameterDataBytes = globalParameters.GetParameterDataBytes();
 			if (parameterDataBytes != null)
 			{
+				parameterDataRestored = true;
 				FilterRecord* filterRecord = (FilterRecord*)filterRecordPtr.ToPointer();
 
 				switch (globalParameters.ParameterDataStorageMethod)
@@ -771,12 +772,11 @@ namespace PSFilterLoad.PSApi
 					default:
 						throw new InvalidEnumArgumentException("ParameterDataStorageMethod", (int)globalParameters.ParameterDataStorageMethod, typeof(GlobalParameters.DataStorageMethod));
 				}
-				parameterDataRestored = true;
 			}
 			byte[] pluginDataBytes = globalParameters.GetPluginDataBytes();
 			if (pluginDataBytes != null)
 			{
-
+				parameterDataRestored = true;
 				switch (globalParameters.PluginDataStorageMethod)
 				{
 					case GlobalParameters.DataStorageMethod.HandleSuite:
@@ -816,7 +816,6 @@ namespace PSFilterLoad.PSApi
 					default:
 						throw new InvalidEnumArgumentException("PluginDataStorageMethod", (int)globalParameters.PluginDataStorageMethod, typeof(GlobalParameters.DataStorageMethod));
 				}
-				pluginDataRestored = true;
 			}
 
 		}

PSFilterShim/PSApi/LoadPsFilter.cs
@@ -738,6 +738,7 @@ namespace PSFilterLoad.PSApi
 			byte[] parameterDataBytes = globalParameters.GetParameterDataBytes();
 			if (parameterDataBytes != null)
 			{
+				parameterDataRestored = true;
 				FilterRecord* filterRecord = (FilterRecord*)filterRecordPtr.ToPointer();
 
 				switch (globalParameters.ParameterDataStorageMethod)
@@ -776,12 +777,11 @@ namespace PSFilterLoad.PSApi
 					default:
 						throw new InvalidEnumArgumentException("ParameterDataStorageMethod", (int)globalParameters.ParameterDataStorageMethod, typeof(GlobalParameters.DataStorageMethod));
 				}
-				parameterDataRestored = true;
 			}
 			byte[] pluginDataBytes = globalParameters.GetPluginDataBytes();
 			if (pluginDataBytes != null)
 			{
-
+				pluginDataRestored = true;
 				switch (globalParameters.PluginDataStorageMethod)
 				{
 					case GlobalParameters.DataStorageMethod.HandleSuite:
@@ -818,7 +818,6 @@ namespace PSFilterLoad.PSApi
 					default:
 						throw new InvalidEnumArgumentException("PluginDataStorageMethod", (int)globalParameters.PluginDataStorageMethod, typeof(GlobalParameters.DataStorageMethod));
 				}
-				pluginDataRestored = true;
 			}
 
 		}

commit 483df55d77e120ecfc91aebf2f07f5401b2b8575

Set the pluginDataRestored field in RestoreParameterHandles
The pluginDataRestored field was ignored due to a regression
introduced in 0e46099ea8742381055013b533b302b9f5f3d4e9.

PSApi/LoadPsFilter.cs
@@ -795,7 +795,7 @@ namespace PSFilterLoad.PSApi
 			byte[] pluginDataBytes = globalParameters.GetPluginDataBytes();
 			if (pluginDataBytes != null)
 			{
-				parameterDataRestored = true;
+				pluginDataRestored = true;
 				switch (globalParameters.PluginDataStorageMethod)
 				{
 					case GlobalParameters.DataStorageMethod.HandleSuite: