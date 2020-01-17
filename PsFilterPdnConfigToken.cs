e3cf08966f4fdb346ca644fa1cb149ec3661be26
Nicholas Hayes <0xC0000054@users.noreply.github.com>
2020-01-16 22:01:19 -0700

    Use auto-implemented properties

--- PsFilterPdnConfigToken.cs
+++ PsFilterPdnConfigToken.cs
@@ -19,80 +19,44 @@ namespace PSFilterPdn
 {
     public sealed class PSFilterPdnConfigToken : PaintDotNet.Effects.EffectConfigToken
     {
-        private Surface dest;
-        private PluginData filterData;
-        private bool runWith32BitShim;
-        private Dictionary<PluginData, ParameterData> filterParameters;
-        private PseudoResourceCollection pseudoResources;
-        private DescriptorRegistryValues descriptorRegistry;
-        private ConfigDialogState dialogState;
-
-        public Surface Dest
-        {
-            get => dest;
-            internal set => dest = value;
-        }
-
-        public PluginData FilterData
-        {
-            get => filterData;
-            internal set => filterData = value;
-        }
-
-        public bool RunWith32BitShim
-        {
-            get => runWith32BitShim;
-            internal set => runWith32BitShim = value;
-        }
-
-        public Dictionary<PluginData, ParameterData> FilterParameters
-        {
-            get => filterParameters;
-            internal set => filterParameters = value;
-        }
-
-        public PseudoResourceCollection PseudoResources
-        {
-            get => pseudoResources;
-            internal set => pseudoResources = value;
-        }
-
-        public DescriptorRegistryValues DescriptorRegistry
-        {
-            get => descriptorRegistry;
-            internal set => descriptorRegistry = value;
-        }
-
-        public ConfigDialogState DialogState
-        {
-            get => dialogState;
-            internal set => dialogState = value;
-        }
-
         public PSFilterPdnConfigToken(Surface dest, PluginData filterData, bool useShim, Dictionary<PluginData, ParameterData> paramData,
             PseudoResourceCollection resources, DescriptorRegistryValues registryValues, ConfigDialogState configDialog)
             : base()
         {
-            this.dest = dest;
-            this.filterData = filterData;
-            runWith32BitShim = useShim;
-            filterParameters = paramData;
-            pseudoResources = resources;
-            descriptorRegistry = registryValues;
-            dialogState = configDialog;
+            Dest = dest;
+            FilterData = filterData;
+            RunWith32BitShim = useShim;
+            FilterParameters = paramData;
+            PseudoResources = resources;
+            DescriptorRegistry = registryValues;
+            DialogState = configDialog;
         }
 
         private PSFilterPdnConfigToken(PSFilterPdnConfigToken copyMe)
             : base(copyMe)
         {
-            dest = copyMe.dest;
-            filterData = copyMe.filterData;
-            runWith32BitShim = copyMe.runWith32BitShim;
-            filterParameters = copyMe.filterParameters;
-            pseudoResources = copyMe.pseudoResources;
-            descriptorRegistry = copyMe.descriptorRegistry;
-            dialogState = copyMe.dialogState;
+            Dest = copyMe.Dest;
+            FilterData = copyMe.FilterData;
+            RunWith32BitShim = copyMe.RunWith32BitShim;
+            FilterParameters = copyMe.FilterParameters;
+            PseudoResources = copyMe.PseudoResources;
+            DescriptorRegistry = copyMe.DescriptorRegistry;
+            DialogState = copyMe.DialogState;
         }
+
+        public Surface Dest { get; internal set; }
+
+        public PluginData FilterData { get; internal set; }
+
+        public bool RunWith32BitShim { get; internal set; }
+
+        public Dictionary<PluginData, ParameterData> FilterParameters { get; internal set; }
+
+        public PseudoResourceCollection PseudoResources { get; internal set; }
+
+        public DescriptorRegistryValues DescriptorRegistry { get; internal set; }
+
+        public ConfigDialogState DialogState { get; internal set; }
 
         public override object Clone()
         {
