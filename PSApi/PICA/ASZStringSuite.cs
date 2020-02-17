b505a8a3674d65556cbd936300f954fa024647d3
Nicholas Hayes <0xC0000054@users.noreply.github.com>
2020-02-16 20:27:22 -0700

    Reduce memory usage in the As*String methods
    The Encoding.GetBytes method has an overload that can write to
    unmanaged memory.

--- PSApi/PICA/ASZStringSuite.cs
+++ PSApi/PICA/ASZStringSuite.cs
@@ -681,7 +681,7 @@ namespace PSFilterLoad.PSApi.PICA
             return 0;
         }
 
-        private int AsUnicodeCString(ASZString zstr, IntPtr str, uint strSize, bool checkStrSize)
+        private unsafe int AsUnicodeCString(ASZString zstr, IntPtr str, uint strSize, bool checkStrSize)
         {
             if (str != IntPtr.Zero)
             {
@@ -699,18 +699,21 @@ namespace PSFilterLoad.PSApi.PICA
                     }
                 }
 
-                byte[] bytes = Encoding.Unicode.GetBytes(value);
+                int byteCount = Encoding.Unicode.GetByteCount(value);
 
-                int lengthInChars = bytes.Length / UnicodeEncoding.CharSize;
-                int lengthWithTerminator = lengthInChars + 1;
+                int lengthInChars = byteCount / UnicodeEncoding.CharSize;
+                uint lengthWithTerminator = (uint)lengthInChars + 1;
 
                 if (strSize < lengthWithTerminator)
                 {
                     return PSError.kASBufferTooSmallErr;
                 }
 
-                Marshal.Copy(bytes, 0, str, bytes.Length);
-                Marshal.WriteInt16(str, bytes.Length, '\0');
+                fixed (char* ptr = value)
+                {
+                    Encoding.Unicode.GetBytes(ptr, value.Length, (byte*)str, byteCount);
+                }
+                Marshal.WriteInt16(str, byteCount, '\0');
 
                 return PSError.kASNoErr;
             }
@@ -743,7 +746,7 @@ namespace PSFilterLoad.PSApi.PICA
             return 0;
         }
 
-        private int AsCString(ASZString zstr, IntPtr str, uint strSize, bool checkStrSize)
+        private unsafe int AsCString(ASZString zstr, IntPtr str, uint strSize, bool checkStrSize)
         {
             if (str != IntPtr.Zero)
             {
@@ -761,17 +764,20 @@ namespace PSFilterLoad.PSApi.PICA
                     }
                 }
 
-                byte[] bytes = Encoding.ASCII.GetBytes(value);
+                int byteCount = Encoding.ASCII.GetByteCount(value);
 
-                int lengthWithTerminator = bytes.Length + 1;
+                uint lengthWithTerminator = (uint)byteCount + 1;
 
                 if (strSize < lengthWithTerminator)
                 {
                     return PSError.kASBufferTooSmallErr;
                 }
 
-                Marshal.Copy(bytes, 0, str, bytes.Length);
-                Marshal.WriteByte(str, bytes.Length, 0);
+                fixed (char* ptr = value)
+                {
+                    Encoding.ASCII.GetBytes(ptr, value.Length, (byte*)str, byteCount);
+                }
+                Marshal.WriteByte(str, byteCount, 0);
 
                 return PSError.kASNoErr;
             }
@@ -804,7 +810,7 @@ namespace PSFilterLoad.PSApi.PICA
             return 0;
         }
 
-        private int AsPascalString(ASZString zstr, IntPtr str, uint strSize, bool checkStrSize)
+        private unsafe int AsPascalString(ASZString zstr, IntPtr str, uint strSize, bool checkStrSize)
         {
             if (str != IntPtr.Zero)
             {
@@ -822,19 +828,22 @@ namespace PSFilterLoad.PSApi.PICA
                     }
                 }
 
-                byte[] bytes = Encoding.ASCII.GetBytes(value);
+                int byteCount = Encoding.ASCII.GetByteCount(value);
 
-                int lengthWithPrefixByte = bytes.Length + 1;
+                uint lengthWithPrefixByte = (uint)byteCount + 1;
 
                 if (strSize < lengthWithPrefixByte)
                 {
                     return PSError.kASBufferTooSmallErr;
                 }
 
-                Marshal.WriteByte(str, (byte)bytes.Length);
-                if (bytes.Length > 0)
+                Marshal.WriteByte(str, (byte)byteCount);
+                if (byteCount > 0)
                 {
-                    Marshal.Copy(bytes, 0, new IntPtr(str.ToInt64() + 1L), bytes.Length);
+                    fixed (char* ptr = value)
+                    {
+                        Encoding.ASCII.GetBytes(ptr, value.Length, (byte*)str + 1, byteCount);
+                    }
                 }
 
                 return PSError.kASNoErr;
