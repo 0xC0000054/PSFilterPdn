0434d1a120a7499158fdc63b735f682227bfd6a5
Nicholas Hayes <0xC0000054@users.noreply.github.com>
2022-03-05 17:08:37 -0700

    Bump copyright year

--- PSFilterShim/PSApi/NativeConstants.cs
+++ PSFilterShim/PSApi/NativeConstants.cs
@@ -4,7 +4,7 @@
 // https://github.com/0xC0000054/PSFilterPdn
 //
 // This software is provided under the Microsoft Public License:
-//   Copyright (C) 2010-2021 Nicholas Hayes
+//   Copyright (C) 2010-2022 Nicholas Hayes
 //
 // See LICENSE.txt for complete licensing and attribution information.
 //
