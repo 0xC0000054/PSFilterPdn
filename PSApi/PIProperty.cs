4c773f7f0a136c16805e983805f85cacea06118b
Nicholas Hayes <0xC0000054@users.noreply.github.com>
2022-03-05 18:12:32 -0700

    Add the value names to the rest of the PIPropertyIDs

--- PSApi/PIProperty.cs
+++ PSApi/PIProperty.cs
@@ -47,15 +47,15 @@ namespace PSFilterLoad.PSApi
     internal static class PIPropertyID
     {
         /// <summary>
-        /// The property giving the plug-in's kind, 8BFM for Photoshop Filters
+        /// The property giving the plug-in's kind, 8BFM for Photoshop Filters - 'kind'
         /// </summary>
         public const uint PIKindProperty = 0x6b696e64U;
         /// <summary>
-        /// Win32 Intel code descriptor, Entrypoint
+        /// Win32 Intel code descriptor, Entrypoint - 'wx86'
         /// </summary>
         public const uint PIWin32X86CodeProperty = 0x77783836U;
         /// <summary>
-        /// Win64 Intel code descriptor, Entrypoint
+        /// Win64 Intel code descriptor, Entrypoint - '8664'
         /// </summary>
         /// <remarks>Taken from the PiPL resources of a 64-bit Photoshop Plugin.</remarks>
         public const uint PIWin64X86CodeProperty = 0x38363634U;
@@ -65,27 +65,27 @@ namespace PSFilterLoad.PSApi
         /// <remarks>Taken from the PiPL resources of an ARM64 Photoshop Plugin.</remarks>
         public const uint PIWin64ARMCodeProperty = 0x77613634U;
         /// <summary>
-        /// Major(int16).Minor(int16) version number
+        /// Major(int16).Minor(int16) version number - 'vers'
         /// </summary>
         public const uint PIVersionProperty = 0x76657273U;
         /// <summary>
-        /// Image modes supported flags. (bitmask)
+        /// Image modes supported flags. (bitmask) - 'mode'
         /// </summary>
         public const uint PIImageModesProperty = 0x6d6f6465U;
         /// <summary>
-        /// Category name that appears on top level menu
+        /// Category name that appears on top level menu - 'catg'
         /// </summary>
         public const uint PICategoryProperty = 0x63617467U;
         /// <summary>
-        /// Menu name
+        /// Menu name - 'name'
         /// </summary>
         public const uint PINameProperty = 0x6e616d65U;
         /// <summary>
-        /// Has Terminology Property
+        /// Has Terminology Property - 'hstm'
         /// </summary>
         public const uint PIHasTerminologyProperty = 0x6873746DU;
         /// <summary>
-        /// FilterCaseInfo Property
+        /// FilterCaseInfo Property - 'fici'
         /// </summary>
         public const uint PIFilterCaseInfoProperty = 0x66696369U;
         /// <summary>
