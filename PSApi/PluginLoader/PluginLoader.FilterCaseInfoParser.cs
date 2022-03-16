ada8abd8a198380f91c2ca7c16e785ac67edb423
Nicholas Hayes <0xC0000054@users.noreply.github.com>
2022-03-16 12:08:28 -0600

    Move the PluginLoader nested classes into their own files

---
+++ PSApi/PluginLoader/PluginLoader.FilterCaseInfoParser.cs
@@ -0,0 +1,149 @@
+﻿/////////////////////////////////////////////////////////////////////////////////
+//
+// Photoshop-compatible filter host Effect plugin for Paint.NET
+// https://github.com/0xC0000054/PSFilterPdn
+//
+// This software is provided under the Microsoft Public License:
+//   Copyright (C) 2010-2022 Nicholas Hayes
+//
+// See LICENSE.txt for complete licensing and attribution information.
+//
+/////////////////////////////////////////////////////////////////////////////////
+
+namespace PSFilterLoad.PSApi
+{
+    internal static partial class PluginLoader
+    {
+        private sealed class FilterCaseInfoResult
+        {
+            public readonly FilterCaseInfoCollection filterCaseInfo;
+            public readonly int propertyLength;
+
+            public FilterCaseInfoResult(FilterCaseInfoCollection filterCaseInfo, int actualArrayLength)
+            {
+                this.filterCaseInfo = filterCaseInfo;
+                propertyLength = actualArrayLength;
+            }
+        }
+
+        private static class FilterCaseInfoParser
+        {
+            public static unsafe FilterCaseInfoResult Parse(byte* ptr, int length)
+            {
+                const int MinLength = 7 * FilterCaseInfo.SizeOf;
+
+                if (length < MinLength)
+                {
+                    return null;
+                }
+
+                FilterCaseInfo[] info = new FilterCaseInfo[7];
+                int offset = 0;
+                int bytesRead;
+                bool filterInfoValid = true;
+
+                for (int i = 0; i < info.Length; i++)
+                {
+                    byte? inputHandling = ParseField(ptr, offset, out bytesRead);
+                    offset += bytesRead;
+
+                    byte? outputHandling = ParseField(ptr, offset, out bytesRead);
+                    offset += bytesRead;
+
+                    byte? flags1 = ParseField(ptr, offset, out bytesRead);
+                    offset += bytesRead;
+
+                    byte? flags2 = ParseField(ptr, offset, out bytesRead);
+                    offset += bytesRead;
+
+                    if (inputHandling.HasValue &&
+                        outputHandling.HasValue &&
+                        flags1.HasValue &&
+                        flags2.HasValue)
+                    {
+                        info[i] = new FilterCaseInfo((FilterDataHandling)inputHandling.Value,
+                                                     (FilterDataHandling)outputHandling.Value,
+                                                     (FilterCaseInfoFlags)flags1.Value,
+                                                     flags2.Value);
+                    }
+                    else
+                    {
+                        filterInfoValid = false;
+                    }
+                }
+
+                return new FilterCaseInfoResult(filterInfoValid ? new FilterCaseInfoCollection(info) : null, offset);
+            }
+
+            private static bool IsHexadecimalChar(char value)
+            {
+                return value >= '0' && value <= '9' ||
+                       value >= 'A' && value <= 'F' ||
+                       value >= 'a' && value <= 'f';
+            }
+
+            private static unsafe byte? ParseField(byte* data, int startOffset, out int fieldLength)
+            {
+                byte value = data[startOffset];
+
+                char c = (char)value;
+                // The FilterCaseInfo resource in Alf's Power Toys contains incorrectly escaped hexadecimal numbers.
+                // The numbers are formatted /x00 instead of \x00.
+                if (c == '/')
+                {
+                    char next = (char)data[startOffset + 1];
+                    if (next == 'x')
+                    {
+                        int offset = startOffset + 2;
+                        // Convert the hexadecimal characters to a decimal number.
+                        char hexChar = (char)data[offset];
+
+                        if (IsHexadecimalChar(hexChar))
+                        {
+                            int fieldValue = 0;
+
+                            do
+                            {
+                                int digit;
+
+                                if (hexChar < 'A')
+                                {
+                                    digit = hexChar - '0';
+                                }
+                                else
+                                {
+                                    if (hexChar >= 'a')
+                                    {
+                                        // Convert the letter to upper case.
+                                        hexChar = (char)(hexChar - 0x20);
+                                    }
+
+                                    digit = 10 + (hexChar - 'A');
+                                }
+
+                                fieldValue = (fieldValue * 16) + digit;
+
+                                offset++;
+                                hexChar = (char)data[offset];
+
+                            } while (IsHexadecimalChar(hexChar));
+
+                            if (fieldValue >= byte.MinValue && fieldValue <= byte.MaxValue)
+                            {
+                                fieldLength = offset - startOffset;
+
+                                return (byte)fieldValue;
+                            }
+                        }
+
+                        fieldLength = 2;
+                        return null;
+                    }
+                }
+
+                fieldLength = 1;
+                return value;
+            }
+        }
+    }
+}
---
+++ PSApi/PluginLoader/PluginLoader.QueryFilter.cs
@@ -0,0 +1,56 @@
+﻿/////////////////////////////////////////////////////////////////////////////////
+//
+// Photoshop-compatible filter host Effect plugin for Paint.NET
+// https://github.com/0xC0000054/PSFilterPdn
+//
+// This software is provided under the Microsoft Public License:
+//   Copyright (C) 2010-2022 Nicholas Hayes
+//
+// See LICENSE.txt for complete licensing and attribution information.
+//
+/////////////////////////////////////////////////////////////////////////////////
+
+using System;
+using System.Collections.Generic;
+
+namespace PSFilterLoad.PSApi
+{
+    internal static partial class PluginLoader
+    {
+        private sealed class QueryFilter
+        {
+            public readonly string fileName;
+            public readonly uint platformEntryPoint;
+            public readonly bool runWith32BitShim;
+            public List<PluginData> plugins;
+
+            /// <summary>
+            /// Initializes a new instance of the <see cref="QueryFilter"/> class.
+            /// </summary>
+            /// <param name="fileName">The file name of the plug-in.</param>
+            /// <param name="platform">The processor architecture that the plug-in was built for.</param>
+            /// <exception cref="System.PlatformNotSupportedException">The processor architecture specified by <paramref name="platform"/> is not supported.</exception>
+            public QueryFilter(string fileName, ProcessorArchitecture platform)
+            {
+                this.fileName = fileName;
+                switch (platform)
+                {
+                    case ProcessorArchitecture.X86:
+                        platformEntryPoint = PIPropertyID.PIWin32X86CodeProperty;
+                        break;
+                    case ProcessorArchitecture.X64:
+                        platformEntryPoint = PIPropertyID.PIWin64X86CodeProperty;
+                        break;
+                    case ProcessorArchitecture.Arm64:
+                        platformEntryPoint = PIPropertyID.PIWin64ARMCodeProperty;
+                        break;
+                    case ProcessorArchitecture.Unknown:
+                    default:
+                        throw new PlatformNotSupportedException($"No platform entry point was defined for { nameof(ProcessorArchitecture) }.{ platform }.");
+                }
+                plugins = new List<PluginData>();
+                runWith32BitShim = platform == ProcessorArchitecture.X86 && ProcessInformation.Architecture != ProcessorArchitecture.X86;
+            }
+        }
+    }
+}
