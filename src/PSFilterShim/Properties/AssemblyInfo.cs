00550422e7b684294ba3fa7fbbbee1264a558321
Nicholas Hayes <0xC0000054@users.noreply.github.com>
2022-10-09 07:16:54 -0600

    Update the references and target framework for Paint.NET 5.0

--- src/PSFilterShim/Properties/AssemblyInfo.cs
+++ src/PSFilterShim/Properties/AssemblyInfo.cs
@@ -13,6 +13,7 @@
 using System.Reflection;
 using System.Runtime.InteropServices;
 using System.Resources;
+using System.Runtime.Versioning;
 
 // General Information about an assembly is controlled through the following
 // set of attributes. Change these attribute values to modify the information
@@ -25,6 +26,8 @@ using System.Resources;
 [assembly: AssemblyCopyright("Copyright © 2010-2022 Nicholas Hayes (aka null54)")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
+
+[assembly: SupportedOSPlatform("windows")]
 
 // Setting ComVisible to false makes the types in this assembly not visible
 // to COM components.  If you need to access a type in this assembly from
