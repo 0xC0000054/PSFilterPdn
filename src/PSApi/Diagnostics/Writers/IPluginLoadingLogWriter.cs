0302dd288ede69519a57458d892855947fba43d0
Nicholas Hayes <0xC0000054@users.noreply.github.com>
2022-10-31 14:25:36 -0600

    Add a few missing file headers

--- src/PSApi/Diagnostics/Writers/IPluginLoadingLogWriter.cs
+++ src/PSApi/Diagnostics/Writers/IPluginLoadingLogWriter.cs
@@ -1,4 +1,16 @@
-﻿namespace PSFilterLoad.PSApi.Diagnostics
+﻿/////////////////////////////////////////////////////////////////////////////////
+//
+// Photoshop-compatible filter host Effect plugin for Paint.NET
+// https://github.com/0xC0000054/PSFilterPdn
+//
+// This software is provided under the Microsoft Public License:
+//   Copyright (C) 2010-2022 Nicholas Hayes
+//
+// See LICENSE.txt for complete licensing and attribution information.
+//
+/////////////////////////////////////////////////////////////////////////////////
+
+namespace PSFilterLoad.PSApi.Diagnostics
 {
     internal interface IPluginLoadingLogWriter
     {
--- src/PSApi/Diagnostics/Writers/PluginLoadingDictionaryLogWriter.cs
+++ src/PSApi/Diagnostics/Writers/PluginLoadingDictionaryLogWriter.cs
@@ -1,4 +1,16 @@
-﻿using System;
+﻿/////////////////////////////////////////////////////////////////////////////////
+//
+// Photoshop-compatible filter host Effect plugin for Paint.NET
+// https://github.com/0xC0000054/PSFilterPdn
+//
+// This software is provided under the Microsoft Public License:
+//   Copyright (C) 2010-2022 Nicholas Hayes
+//
+// See LICENSE.txt for complete licensing and attribution information.
+//
+/////////////////////////////////////////////////////////////////////////////////
+
+using System;
 using System.Collections.Generic;
 
 namespace PSFilterLoad.PSApi.Diagnostics
