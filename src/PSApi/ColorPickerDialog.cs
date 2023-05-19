7696b0d09d12df3544b420392fd4f75665e419e8
Nicholas Hayes <0xC0000054@users.noreply.github.com>
2023-05-19 01:43:56 -0600

    Add a missing file header

--- src/PSApi/ColorPickerDialog.cs
+++ src/PSApi/ColorPickerDialog.cs
@@ -1,4 +1,16 @@
-﻿using PSFilterPdn.Interop;
+﻿/////////////////////////////////////////////////////////////////////////////////
+//
+// Photoshop-compatible filter host Effect plugin for Paint.NET
+// https://github.com/0xC0000054/PSFilterPdn
+//
+// This software is provided under the Microsoft Public License:
+//   Copyright (C) 2010-2023 Nicholas Hayes
+//
+// See LICENSE.txt for complete licensing and attribution information.
+//
+/////////////////////////////////////////////////////////////////////////////////
+
+using PSFilterPdn.Interop;
 using System;
 using System.Runtime.InteropServices;
 
