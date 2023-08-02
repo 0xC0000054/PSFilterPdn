0xC0000054/PSFilterPdn

commit 29c59ab20e52a3fee4409acd9415bf03ab28a96b

Call Dispose in the LoadFilter constructor if an exception is thrown

src/PSApi/LoadPsFilter.cs
@@ -288,9 +288,6 @@ namespace PSFilterLoad.PSApi
             lastOutLoPlane = -1;
             lastInLoPlane = -1;
 
-            this.source = takeOwnershipOfSource ? source : source.Clone();
-            dest = source.Clone();
-
             advanceProc = new AdvanceStateProc(AdvanceStateProc);
             colorProc = new ColorServicesProc(ColorServicesProc);
             displayPixelsProc = new DisplayPixelsProc(DisplayPixelsProc);
@@ -329,22 +326,33 @@ namespace PSFilterLoad.PSApi
 
             readImageDocument = new ReadImageDocument(this.source.Width, this.source.Height, dpiX, dpiY);
 
-            if (selectionMask != null)
+            try
             {
-                if (takeOwnershipOfSelectionMask)
+                this.source = takeOwnershipOfSource ? source : source.Clone();
+                dest = source.Clone();
+
+                if (selectionMask != null)
                 {
-                    mask = selectionMask;
+                    if (takeOwnershipOfSelectionMask)
+                    {
+                        mask = selectionMask;
+                    }
+                    else
+                    {
+                        mask = selectionMask.Clone();
+                    }
+                    hasSelectionMask = true;
                 }
                 else
                 {
-                    mask = selectionMask.Clone();
+                    mask = null;
+                    hasSelectionMask = false;
                 }
-                hasSelectionMask = true;
             }
-            else
+            catch (Exception)
             {
-                mask = null;
-                hasSelectionMask = false;
+                Dispose();
+                throw;
             }
         }
 

commit f717fad02a95bcb939a921ea0536c9ec977f0618

Fix a null reference crash
This was introduced in commit 29c59ab20e52a3fee4409acd9415bf03ab28a96b.

src/PSApi/LoadPsFilter.cs
@@ -304,8 +304,8 @@ namespace PSFilterLoad.PSApi
             propertySuite = new PropertySuite(handleSuite,
                                               documentMetadataProvider,
                                               logger.CreateInstanceForType(nameof(PSApi.PropertySuite)),
-                                              this.source.Width,
-                                              this.source.Height,
+                                              source.Width,
+                                              source.Height,
                                               pluginUISettings);
             resourceSuite = new ResourceSuite(handleSuite, logger.CreateInstanceForType(nameof(ResourceSuite)));
             basicSuiteProvider = new SPBasicSuiteProvider(this,
@@ -324,7 +324,7 @@ namespace PSFilterLoad.PSApi
             this.surfaceFactory = surfaceFactory;
             this.documentMetadataProvider = documentMetadataProvider;
 
-            readImageDocument = new ReadImageDocument(this.source.Width, this.source.Height, dpiX, dpiY);
+            readImageDocument = new ReadImageDocument(source.Width, source.Height, dpiX, dpiY);
 
             try
             {