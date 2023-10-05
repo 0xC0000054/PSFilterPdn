b9ca2484dca1993246f0811e6b4457b5d0d8f07a
Nicholas Hayes <0xC0000054@users.noreply.github.com>
2023-10-04 18:17:57 -0600

    Simplify the IntPtrExtensions ToHexString method

--- src/PSApi/IntPtrExtensions.cs
+++ src/PSApi/IntPtrExtensions.cs
@@ -19,14 +19,9 @@ namespace PSFilterLoad.PSApi
         /// </summary>
         /// <param name="pointer">The pointer.</param>
         /// <returns></returns>
-        public static string ToHexString(this System.IntPtr pointer)
+        public static string ToHexString(this nint pointer)
         {
-            if (System.IntPtr.Size == 8)
-            {
-                return pointer.ToString("X16");
-            }
-
-            return pointer.ToString("X8");
+            return pointer.ToString(nint.Size == 8 ? "X16" : "X8");
         }
     }
 }
