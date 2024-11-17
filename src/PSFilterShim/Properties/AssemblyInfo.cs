78af670d067d58cefc35ee527733dd1801743668
Nicholas Hayes <0xC0000054@users.noreply.github.com>
2024-11-17 04:30:36 -0700

    Release version 2.0.10

--- src/PSFilterShim/Properties/AssemblyInfo.cs
+++ src/PSFilterShim/Properties/AssemblyInfo.cs
@@ -47,6 +47,6 @@ using System.Runtime.Versioning;
 // You can specify all the values or you can default the Build and Revision Numbers
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("2.0.9.0")]
-[assembly: AssemblyFileVersion("2.0.9.0")]
+[assembly: AssemblyVersion("2.0.10.0")]
+[assembly: AssemblyFileVersion("2.0.10.0")]
 [assembly: NeutralResourcesLanguage("en-US")]
