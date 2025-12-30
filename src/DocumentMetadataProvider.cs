b43f7787708e0d18eb676969fb248ba13f449ea2
Nicholas Hayes <0xC0000054@users.noreply.github.com>
2025-12-30 13:01:35 -0700

    Simplify the code for fetching the image color profile
    We now use the IEffectDocumentInfo ColorContext property instead of
    parsing the EXIF properties.
    This makes the plugin's behavior match what other effect plugins would
    use as the color context.

--- src/DocumentMetadataProvider.cs
+++ src/DocumentMetadataProvider.cs
@@ -16,17 +16,12 @@ using PSFilterLoad.PSApi;
 using PSFilterPdn.Metadata;
 using System;
 using System.Collections.Generic;
-using System.IO;
-using System.Linq;
 using System.Text;
 
 namespace PSFilterPdn
 {
     internal sealed class DocumentMetadataProvider : IDocumentMetadataProvider
     {
-        private const string SrgbProfileResourceName = $"{nameof(PSFilterPdn)}.Resources.sRGB.icc";
-        private const string AdobeRGBProfileResourceName = $"{nameof(PSFilterPdn)}.Resources.ClayRGB-elle-V2-g22.icc";
-
         private readonly IEffectDocumentInfo documentInfo;
         private readonly Lazy<byte[]> exifBytes;
         private readonly Lazy<byte[]> iccProfileBytes;
@@ -54,106 +49,7 @@ namespace PSFilterPdn
 
         private byte[] CacheExifBytes() => new ExifWriter(documentInfo.Metadata.ExifPropertyItems).CreateExifBlob();
 
-        private byte[] CacheIccProfileBytes()
-        {
-            ExifPropertyPath colorSpacePath = ExifPropertyKeys.Photo.ColorSpace.Path;
-            ExifPropertyPath iccProfilePath = ExifPropertyKeys.Image.InterColorProfile.Path;
-
-            IReadOnlyList<ExifPropertyItem> exifPropertyItems = documentInfo.Metadata.ExifPropertyItems;
-
-            ExifPropertyItem? iccProfilePropertyItem = exifPropertyItems.FirstOrDefault(p => p.Path == iccProfilePath);
-
-            byte[] iccProfileBytes = [];
-
-            if (iccProfilePropertyItem != null)
-            {
-                iccProfileBytes = [.. iccProfilePropertyItem.Value.Data];
-            }
-            else
-            {
-                // If the image does not have an embedded color profile we try to get the
-                // profile from the EXIF color space value.
-                // Images that do not have the EXIF color space value are assumed to be sRGB.
-                ExifColorSpace colorSpace = ExifColorSpace.Srgb;
-
-                ExifPropertyItem? colorSpacePropertyItem = exifPropertyItems.FirstOrDefault(p => p.Path == colorSpacePath);
-
-                if (colorSpacePropertyItem != null)
-                {
-                    ExifValue exifValue = colorSpacePropertyItem.Value;
-
-                    if (exifValue.Type == ExifValueType.Short)
-                    {
-                        colorSpace = (ExifColorSpace)ExifConverter.DecodeShort(exifValue.Data);
-                    }
-                }
-
-                string resourceName;
-
-                switch (colorSpace)
-                {
-                    case ExifColorSpace.Srgb:
-                        resourceName = SrgbProfileResourceName;
-                        break;
-                    case ExifColorSpace.AdobeRgb:
-                        resourceName = AdobeRGBProfileResourceName;
-                        break;
-                    case ExifColorSpace.Uncalibrated:
-                        resourceName = TryGetExifColorSpaceFromInteropIndex(exifPropertyItems);
-                        break;
-                    default:
-                        resourceName = string.Empty;
-                        break;
-                }
-
-                if (!string.IsNullOrEmpty(resourceName))
-                {
-                    using (Stream? stream = typeof(DocumentMetadataProvider).Assembly.GetManifestResourceStream(resourceName))
-                    {
-                        if (stream != null)
-                        {
-                            iccProfileBytes = new byte[stream.Length];
-                            stream.ReadExactly(iccProfileBytes, 0, iccProfileBytes.Length);
-                        }
-                    }
-                }
-            }
-
-            return iccProfileBytes;
-
-            static string TryGetExifColorSpaceFromInteropIndex(IReadOnlyList<ExifPropertyItem> exifPropertyItems)
-            {
-                // The ExifColorSpace.AdobeRgb value is a non-standard WIC extension, most software only supports
-                // ExifColorSpace.Uncalibrated with the InteropIndex set to R03.
-                // See https://ninedegreesbelow.com/photography/embedded-color-space-information.html
-
-                string profileResourceName = string.Empty;
-
-                ExifPropertyPath interopIndexPath = ExifPropertyKeys.Interop.InteroperabilityIndex.Path;
-
-                ExifPropertyItem? interopIndexPropertyItem = exifPropertyItems.FirstOrDefault(p => p.Path == interopIndexPath);
-
-                if (interopIndexPropertyItem != null)
-                {
-                    ExifValue exifValue = interopIndexPropertyItem.Value;
-
-                    if (exifValue.Type == ExifValueType.Ascii)
-                    {
-                        char[] interopIndexValue = ExifConverter.DecodeAscii(exifValue.Data);
-
-                        if (interopIndexValue.Length == 3
-                            && interopIndexValue[0] == 'R'
-                            && interopIndexValue[1] == '0'
-                            && interopIndexValue[2] == '3')
-                        {
-                            profileResourceName = AdobeRGBProfileResourceName;
-                        }
-                    }
-                }
-
-                return profileResourceName;
-            }
-        }
+        private byte[] CacheIccProfileBytes() => documentInfo.ColorContext.GetProfileBytes().ToArray();
 
         private byte[] CacheIptcCaptionRecordBytes()
         {
