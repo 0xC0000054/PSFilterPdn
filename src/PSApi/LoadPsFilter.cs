0xC0000054/PSFilterPdn

commit 030be2d4170e6de91f0b3c6897c5a414cc4a219c

Move the PlatformData initialization into SetupSuites

src/PSApi/LoadPsFilter.cs
@@ -269,9 +269,6 @@ namespace PSFilterLoad.PSApi
             parentWindowHandle = owner;
             PostProcessingOptions = FilterPostProcessingOptions.None;
 
-            platFormDataPtr = Memory.Allocate(Marshal.SizeOf<PlatformData>(), true);
-            ((PlatformData*)platFormDataPtr.ToPointer())->hwnd = owner;
-
             lastOutRect = Rect16.Empty;
             lastInRect = Rect16.Empty;
             lastMaskRect = Rect16.Empty;
@@ -2803,6 +2800,9 @@ namespace PSFilterLoad.PSApi
 
         private unsafe void SetupSuites()
         {
+            platFormDataPtr = Memory.Allocate(Marshal.SizeOf<PlatformData>(), true);
+            ((PlatformData*)platFormDataPtr.ToPointer())->hwnd = parentWindowHandle;
+
             bufferProcsPtr = bufferSuite.CreateBufferProcsPointer();
 
             handleProcsPtr = handleSuite.CreateHandleProcsPointer();

src/PSFilterShim/PSApi/LoadPsFilter.cs
@@ -283,12 +283,6 @@ namespace PSFilterLoad.PSApi
             abortFunc = null;
             progressFunc = null;
 
-            unsafe
-            {
-                platFormDataPtr = Memory.Allocate(Marshal.SizeOf<PlatformData>(), true);
-                ((PlatformData*)platFormDataPtr)->hwnd = parentWindowHandle;
-            }
-
             lastOutRect = Rect16.Empty;
             lastInRect = Rect16.Empty;
             lastMaskRect = Rect16.Empty;
@@ -2820,6 +2814,9 @@ namespace PSFilterLoad.PSApi
 
         private unsafe void SetupSuites()
         {
+            platFormDataPtr = Memory.Allocate(Marshal.SizeOf<PlatformData>(), true);
+            ((PlatformData*)platFormDataPtr.ToPointer())->hwnd = parentWindowHandle;
+
             bufferProcsPtr = bufferSuite.CreateBufferProcsPointer();
 
             handleProcsPtr = handleSuite.CreateHandleProcsPointer();

commit 0f68ec5addc85db7ebc54f56d2e9a1a0fbbfe573

Fix a crash in PluginAbout
The AboutRecord structure was using a null pointer for the platformData
field due to some refactoring that moved its initialization into
SetupSuites.
The bug was introduced in 030be2d4170e6de91f0b3c6897c5a414cc4a219c.

This was causing many filters to hang or crash when showing their about
boxes.

src/PSApi/LoadPsFilter.cs
@@ -810,9 +810,14 @@ namespace PSFilterLoad.PSApi
 
             basicSuitePtr = basicSuiteProvider.CreateSPBasicSuitePointer();
 
+            PlatformData platformData = new()
+            {
+                hwnd = parentWindowHandle
+            };
+
             AboutRecord aboutRecord = new()
             {
-                platformData = platformDataPtr,
+                platformData = &platformData,
                 sSPBasic = basicSuitePtr,
                 plugInRef = IntPtr.Zero
             };